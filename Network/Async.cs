c725746c83222ffd1c73cfbca6f4e0b4772cdc0e
ALECIO FILIPE <ALEC1O@protonmail.com>
2022-04-13 16:51:57 +0300

    Create Async.cs

---
+++ Network/Async.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zenet.Network
+{
+    public static class Async
+    {       
+        public static void Thread(Action callback)
+        {
+            System.Threading.ThreadPool.QueueUserWorkItem((_) => callback?.Invoke());
+        }
+    }
+}
