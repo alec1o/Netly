6b2072ba25a2ac859e60d3e64b1b48912328191f
Alecio Furanze <alecio@kezero.com>
2022-04-14 18:33:11 +0300

    Update ServerBehaviour.cs

--- Manager/ServerBehaviour.cs
+++ Manager/ServerBehaviour.cs
@@ -1,18 +1,24 @@
-﻿using System;
+﻿#if UNITY_EDITOR || UNITY_STANDALONE || UNITY_IOS || UNITY_WII || UNITY_ANDROID || UNITY_PS4 || UNITY_XBOXONE || UNITY_LUMIN || UNITY_TIZEN || UNITY_TVOS || UNITY_WEBGL || UNITY_ANALYTICS || UNITY_WINRT
+#define UNITY
+#endif
+
+using System;
 using Zenet.Network;
 using Zenet.Package;
 using Zenet.Network.Tcp;
 
 namespace Zenet.Manager
 {
+#if UNITY
+    public abstract class ServerBehaviour : UnityEngine.MonoBehaviour
+#else
     public abstract class ServerBehaviour
+#endif
     {
         public static bool UseEvent { get; set; } = false;
         public static bool SendAsync { get; set; } = true;
         public static Encode UseEncode { get; set; } = Encode.UTF8;
-        public static ServerTCP ServerTCP;
-        public static object ServerUDP;
-        private bool initUDP, initTCP;
+        public static ServerTCP serverTcp { get; private set; } = null;
 
         public virtual void OnOpen(Protocol protocol) { }
         public virtual void OnClose(Protocol protocol) { }
@@ -23,143 +29,60 @@ namespace Zenet.Manager
         public virtual void OnData(Protocol protocol, object client, byte[] data) { }
         public virtual void OnEvent(Protocol protocol, object client, string name, byte[] data) { }
 
-        public void Init()
+        private bool initTCP;
+
+        public void Init(Protocol protocol)
         {
-            InitTCP();
-            InitUDP();
+            if (protocol == Protocol.TCP)
+            {
+                if (initTCP) return;
+
+                serverTcp.OnOpen(() => OnOpen(Protocol.TCP));
+                serverTcp.OnClose(() => OnClose(Protocol.TCP));
+                serverTcp.OnError((e) => OnError(Protocol.TCP, e));
+                serverTcp.OnClientOpen((client) => OnEnter(Protocol.TCP, client));
+                serverTcp.OnClientClose((client) => OnExit(Protocol.TCP, client));
+
+                if (UseEvent)
+                {                   
+                    serverTcp.OnClientReceive((client, data) =>
+                    {
+                        var e = Event.Verify(data, UseEncode);
+
+                        if (e.name == null)
+                        {
+                            OnData(protocol, client, data);
+                        }
+                        else
+                        {
+                            OnEvent(protocol, client, e.name, e.value);
+                        }
+                    });
+                }
+                else
+                {
+                    serverTcp.OnClientReceive((client, data) => OnData(Protocol.TCP, client, data));
+                }
+
+                initTCP = true;
+            }
         }
 
         public void Init(Protocol protocol, Host host)
         {
             if (protocol == Protocol.TCP)
             {
-                ServerTCP = new ServerTCP(host);
                 initTCP = false;
-                InitTCP();
+                serverTcp = new ServerTCP(host);
+                Init(Protocol.TCP);
             }
-            else if (protocol == Protocol.UDP) InitUDP();
-        }
-
-        private void InitTCP()
-        {
-            if (initTCP || ServerTCP == null) return;
-
-            ServerTCP.OnOpen(() => OnOpen(Protocol.TCP));
-            ServerTCP.OnClose(() => OnClose(Protocol.TCP));
-            ServerTCP.OnError((e) => OnError(Protocol.TCP, e));
-
-            ServerTCP.OnClientOpen((client) =>
-            {
-                Callback.Execute(() => OnEnter(Protocol.TCP, client));
-            });
-
-            ServerTCP.OnClientClose((client) =>
-            {
-                Callback.Execute(() => OnExit(Protocol.TCP, client));
-            });
-
-            ServerTCP.OnClientReceive((client, data) =>
-            {
-                Callback.Execute(() => OnData(Protocol.TCP, client, data));
-            });
-
-            initTCP = true;
-        }
-
-        private void InitUDP()
-        {
-            // ...
         }
 
         public void Open(Protocol protocol)
         {
-            if (!initTCP && !initUDP) return;
-
-            switch (protocol)
+            if (protocol == Protocol.TCP)
             {
-                case Protocol.TCP:
-
-                    if (initTCP)
-                    {
-                        ServerTCP.Open();
-                    }
-
-                    break;
-
-                case Protocol.UDP:
-
-                    if (initUDP)
-                    {
-                        // ...
-                    }
-
-                    break;
-
-                default: return;
-            }
-        }
-
-        public void Close(Protocol protocol)
-        {
-            if (!initTCP && !initUDP) return;
-
-            switch (protocol)
-            {
-                case Protocol.TCP:
-
-                    if (initTCP)
-                    {
-                        ServerTCP.Close();
-                    }
-
-                    break;
-
-                case Protocol.UDP:
-
-                    if (initUDP)
-                    {
-                        // ...
-                    }
-
-                    break;
-
-                default: return;
-            }
-        }
-
-        public bool Exist(Protocol protocol)
-        {
-            switch (protocol)
-            {
-                case Protocol.TCP: return initTCP;
-                case Protocol.UDP: return initUDP;
-                default: return false;
-            }
-        }
-
-        public bool Opened(Protocol protocol)
-        {
-            switch (protocol)
-            {
-                case Protocol.TCP:
-
-                    if (initTCP)
-                    {
-                        return ServerTCP.Opened;
-                    }
-
-                    return false;
-
-                case Protocol.UDP:
-
-                    if (initUDP)
-                    {
-                        // ...
-                    }
-
-                    return false;
-
-                default: return false;
+                if (initTCP) serverTcp.Open();
             }
         }
     }
