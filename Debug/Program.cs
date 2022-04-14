5884588710e2855c37ec15e0664ecca4e37ccfdd
ALECIO FILIPE <ALEC1O@protonmail.com>
2022-04-14 18:34:02 +0300

    Update Program.cs

--- Debug/Program.cs
+++ Debug/Program.cs
@@ -1,4 +1,21 @@
 ﻿using Zenet.Debug;
+using Zenet.Network;
+using Zenet.Package;
+using Zenet.Network.Tcp;
+/*
+var s = new ServerTCP(new Host("127.0.0.1", 8080));
+
+s.OnOpen(() => Console.WriteLine("OnOpen"));
+s.OnClose(() => Console.WriteLine("OnClose"));
+s.OnError((e) => Console.WriteLine("OnError"));
+s.OnClientOpen((c) => Console.WriteLine("OnClientOpen"));
+s.OnClientClose((c) => Console.WriteLine("OnClientClose"));
+s.OnClientReceive((c, d) => Console.WriteLine("OnClientReceive"));
+
+s.Open();
+
+
+*/
 
 S_TCP();
 
