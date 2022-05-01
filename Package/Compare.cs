442700c5604b3a08beb629a158e305bd9a1e3e7d
Alecio Furanze <alecio@kezero.com>
2022-05-01 15:31:56 +0300

    Create Compare.cs

---
+++ Package/Compare.cs
@@ -0,0 +1,33 @@
+﻿namespace Zeent.Package
+{
+    public static class Compare
+    {
+        public static bool Int(int value1, int value2)
+        {
+            return value1 == value2;
+        }
+        
+        public static bool Byte(byte value1, byte value2)
+        {
+            return value1 == value2;
+        }
+        
+        public static bool String(string value1, string value2)
+        {
+            return value1 == value2;
+        }
+
+        public static bool Bytes(byte[] value1, byte[] value2)
+        {
+            if (value1 == value2) return true;
+            if (value1 == null || value2 == null || value1.Length != value2.Length) return false;
+
+            for (int i = 0; i < value1.Length; i++)
+            {
+                if (value1[i] != value2[i]) return false;
+            }
+
+            return true;
+        }
+    }
+}
