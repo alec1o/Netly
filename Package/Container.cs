46557e3b575b80f4943520428812e75428549aa9
Alecio Furanze <alecio@kezero.com>
2022-05-20 04:42:44 +0300

    Add->long Implemented

--- Package/Container.cs
+++ Package/Container.cs
@@ -44,7 +44,7 @@ namespace Zenet.Package
 
         public void Add(long value)
         {
-            throw new NotImplementedException();
+            _data.Add(BitConverter.GetBytes(value));
         }
 
         public void Add(float value)
