f3f101d1705389f0df878eccc19b9fdc076956fb
Alecio Furanze <alecio@kezero.com>
2022-05-20 03:10:15 +0300

    Create IContainer.cs

---
+++ Package/IContainer.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zenet.Package
+{
+    public interface IContainer
+    {
+        /// <summary>
+        /// Is the deserialization index
+        /// </summary>
+        int Index { get; }
+
+        /// <summary>
+        /// It is the result of serialization
+        /// </summary>
+        byte[] Serialized { get; }
+
+        /// <summary>
+        /// Data that will be used to deserialize
+        /// </summary>
+        byte[] Deserialize { set; }
+
+        /// <summary>
+        /// Contains all deserialize error
+        /// </summary>
+        string[] Errors { get; }
+
+        #region Add
+
+        // integer numbers
+        void Add(byte value);
+        void Add(int value);
+        void Add(uint value);
+        void Add(short value);
+        void Add(ushort value);
+        void Add(long value);
+        void Add(ulong value);
+
+        // real numbers  
+        void Add(float value);
+        void Add(decimal value);
+        void Add(double value);
+
+        // text
+        void Add(string value);
+        void Add(char value);
+        void Add(bool value);
+
+        // others
+        void Add(byte[] value);
+        void Add(Vec3 value);
+        void Add(Vec2 value);
+        void Add(DateTime value);
+
+        #endregion#region Add
+
+        #region Get
+
+        // integer numbers
+        byte GetByte();
+        int GetInt();
+        uint GetUInt();
+        short GetShort();
+        ushort GetUShort();
+        long GetLong();
+        ulong GetULong();
+
+        // real numbers  
+        float GetFloat();
+        decimal GetDecimal();
+        double GetDouble();
+
+        // text
+        string GetString();
+        char GetChar();
+        bool GetBool();
+
+        // others
+        byte[] GetBytes();
+        Vec3 GetVec3();
+        Vec2 GetVec2();
+        DateTime GetDateTime();
+
+        #endregion
+    }
+}
