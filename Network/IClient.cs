4d4ee6b48bd6882afdc22993e8428ae66cd09d78
Alecio Furanze <alecio@kezero.com>
2022-05-21 02:10:25 +0300

    Create IClient.cs

---
+++ Network/IClient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Zenet.Network
+{
+    public interface IClient
+    {
+        Socket Socket { get; }
+        Host Host { get; }
+        bool Opened { get; }
+
+        // Controller
+        void Open(Host host);
+        void Close();
+        void ToData(byte[] data);
+        void ToEvent(string name, byte[] data);
+
+        // Events
+        void OnOpen(Action callback);
+        void OnError(Action callback);
+        void OnClose(Action callback);
+        void OnData(Action<byte[]> callback);
+        void OnEvent(Action<string, byte[]> callback);
+    }
+}
