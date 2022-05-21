2c4f1175e2d683c3635c0ca08b0f0e04970083cd
Alecio Furanze <alecio@kezero.com>
2022-05-21 16:38:26 +0300

    OnOpen Implemented

--- Tcp/TcpServer.cs
+++ Tcp/TcpServer.cs
@@ -132,7 +132,13 @@ namespace Zenet.Tcp
 
         public void OnOpen(Action callback)
         {
-            throw new NotImplementedException();
+            _OnOpen += (_, __) =>
+            {
+                Callback.Execute(() =>
+                {
+                    callback?.Invoke();
+                });
+            };
         }
 
         public void OnError(Action<Exception> callback)
