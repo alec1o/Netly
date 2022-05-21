87af979297c6c5b8cf590ea5cf63aafe15135bc4
Alecio Furanze <alecio@kezero.com>
2022-05-21 04:19:37 +0300

    organize the project

--- Tcp/TcpServer.cs
+++ Tcp/TcpServer.cs
@@ -4,43 +4,90 @@ using System.Text;
 using Zenet.Package;
 using Zenet.Network;
 using System.Net.Sockets;
+using System.Net;
 
 namespace Zenet.Tcp
 {
     public class TcpServer : IServer
     {
-        public Socket Socket => throw new NotImplementedException();
+        #region Private
 
-        public Host Host => throw new NotImplementedException();
+        private Socket _socket { get; set; }
+        private Host _host { get; set; }
 
-        public bool Opened => throw new NotImplementedException();
+        private bool _tryOpen { get; set; }
+        private bool _tryClose { get; set; }
+
+        private EventHandler _OnOpen { get; set; }
+        private EventHandler<Exception> _OnError { get; set; }
+        private EventHandler _OnClose { get; set; }
+        private EventHandler<object> _OnEnter { get; set; }
+        private EventHandler<object> _OnExit { get; set; }
+        private EventHandler<(object client, byte[] data)> _OnData { get; set; }
+        private EventHandler<(object client, string name, byte[] data)> _OnEvent { get; set; }
+
+        #endregion
+
+        #region Public
+
+        public Socket Socket => _socket;
+
+        public Host Host => _host;
+
+        public bool Opened => VerifyOpened();
+
+        public List<object> Clients { get; private set; }
+
+        #endregion
+
+        #region Init
+
+        public TcpServer()
+        {
+            _host = new Host(IPAddress.Any, 0);
+            _socket = new Socket(_host.Family, SocketType.Stream, ProtocolType.Tcp);
+            Clients = new List<object>();
+        }
+
+
+        #endregion
+
+        #region Func
+
+        private bool VerifyOpened()
+        {
+            return false;
+        }
+
+        private void BeginAccept()
+        {
+
+        }
+
+        private void EndAccept(IAsyncResult result)
+        {
+
+        }
+
+        #endregion
+
+        #region Remote
+
+        public void Open(Host host)
+        {
+            throw new NotImplementedException();
+        }
 
         public void Close()
         {
             throw new NotImplementedException();
         }
 
-        public void OnClose(Action callback)
-        {
-            throw new NotImplementedException();
-        }
+        #endregion
 
-        public void OnData(Action<object, byte[]> callback)
-        {
-            throw new NotImplementedException();
-        }
+        #region Client
 
         public void OnEnter(Action<object> callback)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void OnError(Action<Exception> callback)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void OnEvent(Action<object, string, byte[]> callback)
         {
             throw new NotImplementedException();
         }
@@ -50,14 +97,35 @@ namespace Zenet.Tcp
             throw new NotImplementedException();
         }
 
+        public void OnData(Action<object, byte[]> callback)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnEvent(Action<object, string, byte[]> callback)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region Events
+
         public void OnOpen(Action callback)
         {
             throw new NotImplementedException();
         }
 
-        public void Open(Host host)
+        public void OnError(Action<Exception> callback)
         {
             throw new NotImplementedException();
         }
+
+        public void OnClose(Action callback)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion        
     }
 }
