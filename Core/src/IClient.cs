592faf368535aaeafd5e0830ed5418a83c89f58f
Alecio Furanze <alecio@kezero.com>
2022-05-24 23:25:41 +0300

    Restored Project Architecture [ v0.9 ]

---
+++ Core/src/IClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace Zenet.Core
+{
+    public interface IClient
+    {
+        Socket Socket { get; }
+        ZHost Host { get; }
+        bool Opened { get; }
+        string Id { get; }
+
+        // Controller
+        void Open(ZHost host);
+        void Close();
+        void ToData(byte[] data);
+        void ToEvent(string name, byte[] data);
+
+        // Events
+        void OnOpen(Action callback);
+        void OnError(Action<Exception> callback);
+        void OnClose(Action callback);
+        void OnData(Action<byte[]> callback);
+        void OnEvent(Action<string, byte[]> callback);
+    }
+}
---
+++ Core/src/IServer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Zenet.Core
+{
+    public interface IServer
+    {
+        Socket Socket { get; }
+        ZHost Host { get; }
+        bool Opened { get; }
+
+        // Controller
+        void Open(ZHost host);
+        void Close();
+
+        // Events        
+        void OnOpen(Action callback);
+        void OnError(Action<Exception> callback);
+        void OnClose(Action callback);
+
+        // Client
+        /*
+        void OnEnter(Action<object> callback);
+        void OnExit(Action<object> callback);
+        void OnData(Action<object, byte[]> callback);
+        void OnEvent(Action<object, string, byte[]> callback);
+        */
+    }
+}
