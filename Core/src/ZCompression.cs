25af8d8d12ec0b979f52165ef297842f0344a795
Alecio Furanze <alecio@kezero.com>
2022-06-26 12:13:33 +0200

    Adding compression with GZIP

--- Core/src/ZCompression.cs
+++ Core/src/ZCompression.cs
@@ -1,51 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Text;
 
 namespace Zenet.Core
 {
     public static class ZCompression
     {
-        public enum Mode { ZLIB = 0, DEFLATE = 1, GZIP = 2, GZIP2 = 3 }
+        public enum Mode { GZIP = 0 }
 
-        public static byte[] Compress(byte[] data, Mode mode)
+        public static byte[] StringCompress(string data, Mode mode = Mode.GZIP)
+        {
+            return Compress(ZEncoding.ToBytes(data, ZEncode.UNICODE), mode);
+        }
+
+        public static byte[] Compress(byte[] data, Mode mode = Mode.GZIP)
         {
             if (data == null || data.Length <= 0) return data;
 
             switch (mode)
             {
-                case Mode.ZLIB: return ToZLIB(data);
-                case Mode.DEFLATE: return null;
-                case Mode.GZIP: return null;
-                case Mode.GZIP2: return null;
+                case Mode.GZIP: return ToGZip(data);
                 default: return null;
             }
         }
 
-        public static byte[] Decompress(byte[] data, Mode mode)
+        public static string StringDecompress(byte[] data, Mode mode = Mode.GZIP)
+        {
+            return ZEncoding.ToString(Decompress(data, mode), ZEncode.UNICODE);
+        }
+
+        public static byte[] Decompress(byte[] data, Mode mode = Mode.GZIP)
         {
             if (data == null || data.Length <= 0) return data;
 
             switch (mode)
             {
-                case Mode.ZLIB: return FromZLIB(data);
-                case Mode.DEFLATE: return null;
-                case Mode.GZIP: return null;
-                case Mode.GZIP2: return null;
+                case Mode.GZIP: return FromGZip(data);
                 default: return null;
             }
         }
 
-        #region ZLIB
+        #region GZip
 
-        private static byte[] ToZLIB(byte[] data)
+        private static byte[] ToGZip(byte[] data)
         {
-            return null;
+            using (var s = new MemoryStream())
+            {
+                using (var g = new GZipStream(s, CompressionMode.Compress))
+                {
+                    g.Write(data, 0, data.Length);
+                    g.Close();
+                    return s.ToArray();
+                }
+            }
         }
 
-        private static byte[] FromZLIB(byte[] data)
+        private static byte[] FromGZip(byte[] data)
         {
-            return null;
+            using (var compressedStream = new MemoryStream(data))
+            {
+                using (var zipStream = new GZipStream(compressedStream, CompressionMode.Decompress))
+                {
+                    using (var resultStream = new MemoryStream())
+                    {
+                        zipStream.CopyTo(resultStream);
+                        return resultStream.ToArray();
+                    }
+                }
+            }
         }
 
         #endregion
