2b5faa68f2e20a12986b582849c53cdd2e350650
Alecio Furanze <alecio@kezero.com>
2022-07-11 22:41:30 +0200

    Create Async.cs

---
+++ Netly.Core/Resource/Async.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace Netly.Core
+{
+    /// <summary>
+    /// A module that abstracts and executes code in the background
+    /// </summary>
+    public static class Async
+    {
+        /// <summary>
+        /// Execute an (Action) using (Safe ThreadPool)
+        /// </summary>
+        /// <param name="action">Action / Callback</param>
+        /// <returns></returns>
+        public static bool SafePool(Action action)
+        {
+            return ThreadPool.QueueUserWorkItem(_ => action(), null);
+        }
+
+        /// <summary>
+        /// Execute an (Action) using (Unsafe ThreadPool)
+        /// </summary>
+        /// <param name="action">Action / Callback</param>
+        /// <returns></returns>
+        public static bool UnsafePool(Action action)
+        {
+            return ThreadPool.UnsafeQueueUserWorkItem(_ => action(), null);
+        }
+    }
+}
