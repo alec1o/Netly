93c2a75b9d84702c4ec4f81cc6237f3b8ef6a669
Alecio Furanze <alecio@kezero.com>
2023-02-12 06:31:59 +0200

    Create IUdpServer.cs

---
+++ src/Core/IUdpServer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Sockets;
+using TcpClient = Netly.TcpClient;
+
+namespace Netly
+{
+    internal interface IUdpServer
+    {
+        Host Host { get; }
+        bool IsOpened { get; }
+
+        void Open(Host host);
+        void OnError(Action<Exception> callback);
+        void OnOpen(Action callback);
+
+        void Close();
+        void OnClose(Action callback);
+
+        void OnEnter(Action<UdpClient> callback);
+        void OnExit(Action<UdpClient> callback);
+
+        void OnData(Action<UdpClient, byte[]> callback);
+        void OnEvent(Action<UdpClient, string, byte[]> callback);
+
+        void OnModify(Action<Socket> callback);
+    }
+}
