3a6d4dc280d3dbe157fc5dbac78d90892bc9b41f
Alecio Furanze <alecio@kezero.com>
2023-02-13 00:24:31 +0200

    re-create it

--- src/Tcp/TcpClient.cs
+++ src/Tcp/TcpClient.cs
@@ -14,99 +14,49 @@ namespace Netly
     /// </summary>
     public class TcpClient : IClient
     {
-        #region Var        
-
-        /// <summary>
-        /// Host
-        /// </summary>
         public Host Host { get; private set; }
-
-
-        /// <summary>
-        /// Returns true if socket is connected
-        /// </summary>
         public bool IsOpened { get => Connected(); }
 
-        internal string Id;
-
+        internal string uid;
         private Socket _socket;
 
-        private bool _isServer;
-        private bool _tryOpen;
-        private bool _tryClose;
-        private bool _invokeClose;
+        private bool _closed;
+        private bool _closing;
+        private bool _connecting;
+        private bool _serverMode;
 
-        private EventHandler _OnOpen;
-        private EventHandler _OnClose;
-        private EventHandler<Exception> _OnError;
-        private EventHandler<byte[]> _OnData;
-        private EventHandler<(string name, byte[] data)> _OnEvent;
-        private EventHandler<Socket> _OnModify;
-
-        #endregion
+        private EventHandler<Socket> onModifyHandler;
+        private EventHandler<byte[]> onDataHandler;
+        private EventHandler<Exception> onErrorHandler;
+        private EventHandler onOpenHandler, onCloseHandler;
+        private EventHandler<(string name, byte[] data)> onEventHandler;
 
 
-        #region Builder
-
-        /// <summary>
-        /// Netly: TcpClient
-        /// </summary>
         public TcpClient()
         {
-            Id = string.Empty;
-            Host = new Host(IPAddress.Any, 0);
-            _isServer = false;
-            _socket = new Socket(Host.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            _serverMode = false;
         }
 
         internal TcpClient(string id, Socket socket)
         {
-            Id = id;
-            _isServer = true;
+            uid = id;
             _socket = socket;
-            _socket.NoDelay = true;
-            Host = new Host(socket.RemoteEndPoint ?? new IPEndPoint(IPAddress.Any, 0));
+            _serverMode = true;
+            Host = new Host(socket.RemoteEndPoint);
         }
 
-        internal void InitServer()
+
+        internal void InitServerMode()
         {
-            _OnOpen?.Invoke(null, null);
+            onOpenHandler?.Invoke(null, null);
             Receive();
         }
 
-        #endregion
-
-        #region Customization Event
-
-        /// <summary>
-        /// Is called, executes action before socket connect
-        /// </summary>
-        /// <param name="callback">action/callback</param>
-        public void OnModify(Action<Socket> callback)
-        {
-            _OnModify += (sender, socket) =>
-            {
-                MainThread.Add(() =>
-                {
-                    callback?.Invoke(socket);
-                });
-            };
-        }
-
-
-        #endregion
-
-        #region Init
-
-        /// <summary>
-        /// Use to open connection
-        /// </summary>
-        /// <param name="host">Endpoint</param>
         public void Open(Host host)
         {
-            if (IsOpened || _tryOpen || _tryClose || _isServer) return;
+            if (IsOpened || _connecting || _closing || _serverMode) return;
 
-            _tryOpen = true;
+            _connecting = true;
 
             ThreadPool.QueueUserWorkItem(_ =>
             {
@@ -116,35 +66,32 @@ namespace Netly
 
                     _socket.NoDelay = true;
 
-                    _OnModify?.Invoke(null, _socket);
+                    onModifyHandler?.Invoke(null, _socket);
 
                     _socket.Connect(host.EndPoint);
 
                     Host = host;
 
-                    _invokeClose = false;
+                    _closed = false;
 
-                    _OnOpen?.Invoke(null, null);
+                    onOpenHandler?.Invoke(null, null);
 
                     Receive();
                 }
                 catch (Exception e)
                 {
-                    _OnError?.Invoke(null, e);
+                    onErrorHandler?.Invoke(null, e);
                 }
 
-                _tryOpen = false;
+                _connecting = false;
             });
         }
 
-        /// <summary>
-        /// Use to close connection
-        /// </summary>
         public void Close()
         {
-            if (!IsOpened || _tryOpen || _tryClose) return;
+            if (!IsOpened || _connecting || _closing) return;
 
-            _tryClose = true;
+            _closing = true;
 
             _socket.Shutdown(SocketShutdown.Both);
 
@@ -159,61 +106,36 @@ namespace Netly
                 {
                     _socket = null;
 
-                    if (!_invokeClose)
+                    if (!_closed)
                     {
-                        _invokeClose = true;
-                        _OnClose?.Invoke(null, null);
+                        _closed = true;
+                        onCloseHandler?.Invoke(null, null);
                     }
                 }
 
-                _tryClose = false;
+                _closing = false;
             });
         }
 
-        /// <summary>
-        /// Use to send raw data
-        /// </summary>
-        /// <param name="value">raw data</param>
         public void ToData(byte[] value)
         {
-            if (_invokeClose) return;
+            if (_closed) return;
 
-            List<byte[]> list = new List<byte[]>
-            {
-                BitConverter.GetBytes(value.Length),
-                value
-            };
+            byte[] buffer = BufferParser.SetPrefix(ref value);
 
-            byte[] buffer = list.SelectMany(b => b).ToArray();
-
-            _socket.Send(buffer, SocketFlags.None);
-            Console.WriteLine($"\t<SEND {buffer.Length} Bytes>");
+            _socket.Send(buffer, 0, buffer.Length, SocketFlags.None);
         }
 
-        /// <summary>
-        /// Use to send event (certain event) 
-        /// </summary>
-        /// <param name="name">event name</param>
-        /// <param name="value">event data</param>
         public void ToEvent(string name, byte[] value)
         {
             ToData(MessageParser.Create(name, value));
         }
 
-        /// <summary>
-        /// Use to send raw data
-        /// </summary>
-        /// <param name="value">raw data</param>
         public void ToData(string value)
         {
             this.ToData(NE.GetBytes(value, NE.Mode.UTF8));
         }
 
-        /// <summary>
-        /// Use to send event (certain event)
-        /// </summary>
-        /// <param name="name">event name</param>
-        /// <param name="value">event data</param>
         public void ToEvent(string name, string value)
         {
             this.ToEvent(name, NE.GetBytes(value, NE.Mode.UTF8));
@@ -238,15 +160,13 @@ namespace Netly
             int length = 0;
             byte[] buffer = new byte[1024 * 8];
 
-
             ThreadPool.QueueUserWorkItem(_ =>
             {
-                while (!_invokeClose)
+                while (!_closed)
                 {
                     try
                     {
-                        length = _socket.Receive(buffer, SocketFlags.None);
-                        Console.WriteLine($"\t<RECEIVE {length} Bytes>");
+                        length = _socket.Receive(buffer, 0, buffer.Length, SocketFlags.None);
 
                         if (length <= 0)
                         {
@@ -257,31 +177,17 @@ namespace Netly
                         byte[] data = new byte[length];
                         Array.Copy(buffer, 0, data, 0, data.Length);
 
-                        int m_size = BitConverter.ToInt32(data, 0);
-                        if (m_size == data.Length - sizeof(Int32))
+                        List<byte[]> messages = BufferParser.GetMessages(ref data);
+
+                        foreach (byte[] message in messages)
                         {
-                            Deploy(data);
+                            (string name, byte[] buffer) content = MessageParser.Verify(message);
+
+                            if (content.buffer == null)
+                                onDataHandler?.Invoke(null, data);
+                            else
+                                onEventHandler?.Invoke(null, (content.name, content.buffer));
                         }
-                        else
-                        {
-                            int _index = 0;
-                            while (_index < data.Length)
-                            {
-                                int _size = BitConverter.ToInt32(data, _index);
-                                _index += sizeof(Int32);
-
-                                Console.WriteLine("TRY RECEIVE: " + _size);
-
-                                byte[] _data = new byte[_size];
-                                Array.Copy(data, _index + (sizeof(Int16)), _data, 0, _data.Length);
-                                _index += _size;
-
-                                Deploy(_data);
-                            }
-                            Console.WriteLine("FINISH RECEIVE");
-                        }
-
-
                     }
                     catch
                     {
@@ -293,104 +199,61 @@ namespace Netly
                     }
                 }
 
-                if (!_tryClose || !_invokeClose)
+                if (!_closing || !_closed)
                 {
-                    _invokeClose = true;
-                    _OnClose?.Invoke(null, null);
+                    _closed = true;
+                    onCloseHandler?.Invoke(null, null);
                 }
             });
 
         }
 
-        private void Deploy(byte[] data)
-        {
-            (string name, byte[] data) result = MessageParser.Verify(data);
-
-            if (result.data == null)
-                _OnData?.Invoke(null, data);
-            else
-                _OnEvent?.Invoke(null, (result.name, result.data));
-        }
-
-        #endregion
-
-        #region Events
-
-        /// <summary>
-        /// Execute the callback, when: the connection is opened
-        /// </summary>
-        /// <param name="callback">action/callback</param>
         public void OnOpen(Action callback)
         {
-            _OnOpen += (sender, args) =>
+            onOpenHandler += (_, __) =>
             {
-                MainThread.Add(() =>
-                {
-                    callback?.Invoke();
-                });
+                MainThread.Add(() => callback?.Invoke());
             };
         }
 
-        /// <summary>
-        /// Execute the callback, when: the connection is closed
-        /// </summary>
-        /// <param name="callback">action/callback</param>
         public void OnClose(Action callback)
         {
-            _OnClose += (sender, args) =>
+            onCloseHandler += (_, __) =>
             {
-                MainThread.Add(() =>
-                {
-                    callback?.Invoke();
-                });
+                MainThread.Add(() => callback?.Invoke());
             };
         }
 
-        /// <summary>
-        /// Execute the callback, when: the connection cannot be opened
-        /// </summary>
-        /// <param name="callback">action/callback</param>
         public void OnError(Action<Exception> callback)
         {
-            _OnError += (sender, exception) =>
+            onErrorHandler += (_, exception) =>
             {
-                MainThread.Add(() =>
-                {
-                    callback?.Invoke(exception);
-                });
+                MainThread.Add(() => callback?.Invoke(exception));
             };
         }
 
-        /// <summary>
-        /// Execute the callback, when: when the connection receives raw data
-        /// </summary>
-        /// <param name="callback">action/callback</param>
         public void OnData(Action<byte[]> callback)
         {
-            _OnData += (sender, data) =>
+            onDataHandler += (_, buffer) =>
             {
-                MainThread.Add(() =>
-                {
-                    callback?.Invoke(data);
-                });
+                MainThread.Add(() => callback?.Invoke(buffer));
             };
         }
 
-        /// <summary>
-        /// Execute the callback, when: when the connection receives event data
-        /// </summary>
-        /// <param name="callback">action/callback</param>
         public void OnEvent(Action<string, byte[]> callback)
         {
-            _OnEvent += (sender, result) =>
+            onEventHandler += (_, content) =>
             {
-                MainThread.Add(() =>
-                {
-                    callback?.Invoke(result.name, result.data);
-                });
+                MainThread.Add(() => callback?.Invoke(content.name, content.data));
             };
         }
 
-        #endregion
+        public void OnModify(Action<Socket> callback)
+        {
+            onModifyHandler += (_, socket) =>
+            {
+                MainThread.Add(() => callback?.Invoke(socket));
+            };
+        }
     }
 }
