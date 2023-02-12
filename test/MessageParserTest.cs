de8ede0298765fb93bc17c9db741f567f2766a8d
Alecio Furanze <alecio@kezero.com>
2023-02-12 23:51:11 +0200

    Create MessageParserTest.cs

---
+++ test/MessageParserTest.cs
@@ -0,0 +1,22 @@
+﻿namespace test;
+using Netly.Core;
+
+public class MessageParserTest
+{
+    [Fact]
+    public void EndToEnd()
+    {
+        string eventName = "sample";
+        byte[] eventData = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        byte[] buffer = MessageParser.Create(eventName, eventData);
+
+        var result = MessageParser.Verify(buffer);
+
+        string bufferName = result.name;
+        byte[] bufferData = result.data;
+
+        Assert.Equal(eventName, bufferName);
+        Assert.Equal(eventData, bufferData);
+    }
+}
