5a35c80f0f6647904d79e53e66ad69397dfc3ba9
Alecio Furanze <alecio@kezero.com>
2023-02-12 05:14:38 +0200

    recreate core files

---
+++ src/Core/IClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace Netly
+{
+    internal interface IClient
+    {
+        Host Host { get; }
+        bool IsOpened { get; }
+
+        void Open(Host host);
+        void OnError(Action<Exception> callback);
+        void OnOpen(Action callback);
+
+        void Close();
+        void OnClose(Action callback);
+
+        void ToData(byte[] value);
+        void OnData(Action<byte[]> callback);
+
+        void ToEvent(string name, byte[] value);
+        void OnEvent(Action<string, byte[]> callback);
+
+        void OnModify(Action<Socket> callback);
+    }
+}
