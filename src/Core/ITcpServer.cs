dcaf7c0760fe9d61e3d7704753fe04c0ae02e259
Alecio Furanze <alecio@kezero.com>
2023-02-12 06:32:03 +0200

    Create ITcpServer.cs

---
+++ src/Core/ITcpServer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Sockets;
+using TcpClient = Netly.TcpClient;
+
+namespace Netly
+{
+    internal interface ITcpServer
+    {
+        Host Host { get; }
+        bool IsOpened { get; }
+
+        void Open(Host host);
+        void OnError(Action<Exception> callback);
+        void OnOpen(Action callback);
+
+        void Close();
+        void OnClose(Action callback);
+
+        void OnEnter(Action<TcpClient> callback);
+        void OnExit(Action<TcpClient> callback);
+
+        void OnData(Action<TcpClient, byte[]> callback);
+        void OnEvent(Action<TcpClient, string, byte[]> callback);
+
+        void OnModify(Action<Socket> callback);
+    }
+}
