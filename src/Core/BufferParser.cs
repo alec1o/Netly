30aca9a1219475aeaa189ab8d1a3450277cb53b1
Alecio Furanze <alecio@kezero.com>
2023-02-13 00:16:23 +0200

    only accept ref

--- src/Core/BufferParser.cs
+++ src/Core/BufferParser.cs
@@ -6,14 +6,14 @@ namespace Netly.Core
 {
     public static class BufferParser
     {
-        public static byte[] SetPrefix(byte[] buffer)
+        public static byte[] SetPrefix(ref byte[] buffer)
         {
             List<byte[]> result = new List<byte[]> { BitConverter.GetBytes((int)(buffer.Length)), buffer };
 
             return result.SelectMany(x => x).ToArray();
         }
 
-        public static List<byte[]> GetMessages(byte[] buffer)
+        public static List<byte[]> GetMessages(ref byte[] buffer)
         {
             int index = 0;
             List<byte[]> messages = new List<byte[]>();
