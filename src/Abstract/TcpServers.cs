7751ca6862c75a1fac0fc61ad0b9181c8738d0dc
Alecio Furanze <alecio@kezero.com>
2023-02-19 22:18:30 +0200

    override Open implementation

--- src/Abstract/TcpServers.cs
+++ src/Abstract/TcpServers.cs
@@ -7,5 +7,41 @@ namespace Netly.Abstract
 {
     public class TcpServers : Server<TcpClient>, IServer<TcpClient>
     {
+        public override void Open(Host host, int backlog)
+        {
+            Console.WriteLine($"TRY OPEN 1: {IsOpened}, {m_tryOpen}, {m_tryClose}");
+            if (IsOpened || m_tryOpen || m_tryClose) return;
+            Console.WriteLine($"TRY OPEN 2: {IsOpened}, {m_tryOpen}, {m_tryClose}");
+
+            m_tryOpen = true;
+
+            ThreadPool.QueueUserWorkItem(_ =>
+            {
+                try
+                {
+                    m_socket = new Socket(host.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                    onModifyHandler?.Invoke(null, m_socket);
+
+                    m_socket.Bind(host.EndPoint);
+                    m_socket.Listen(backlog);
+
+                    Host = host;
+
+                    m_opened = true;
+                    m_invokeClose = false;
+
+                    onOpenHandler?.Invoke(null, null);
+
+                    AcceptOrReceive();
+                }
+                catch (Exception e)
+                {
+                    onErrorHandler?.Invoke(null, e);
+                }
+
+                m_tryOpen = false;
+            });
+        }
     }
 }
