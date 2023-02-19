5067d9cb3b6ad6c08b975bc62a4aecae88b528a3
Alecio Furanze <alecio@kezero.com>
2023-02-19 22:10:22 +0200

    create props

--- src/Abstract/Server.cs
+++ src/Abstract/Server.cs
@@ -8,3 +8,26 @@ namespace Netly.Abstract
 {
     public abstract class Server<T>
     {
+        #region Props
+
+        public Host Host { get; protected set; }
+        public List<T> Clients { get; protected set; } = new List<T>();
+        public bool IsOpened => IsConnected();
+
+        protected Socket m_socket;
+        protected bool m_tryOpen;
+        protected bool m_tryClose;
+        protected bool m_invokeClose;
+        protected bool m_opened;
+
+        protected private EventHandler onOpenHandler;
+        protected private EventHandler<Exception> onErrorHandler;
+        protected private EventHandler onCloseHandler;
+        protected private EventHandler<T> onEnterHandler;
+        protected private EventHandler<T> onExitHandler;
+        protected private EventHandler<(T client, byte[] buffer)> onDataHandler;
+        protected private EventHandler<(T client, string name, byte[] buffer)> onEventHandler;
+        protected private EventHandler<Socket> onModifyHandler;
+        protected readonly object m_lock = new object();
+
+        #endregion
