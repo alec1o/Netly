66fd2cef79680a215eebb9363bd40650559c9dcc
Alecio Furanze <alecio@kezero.com>
2023-02-19 18:55:40 +0200

    implementation of OnError

--- src/Abstract/Client.cs
+++ src/Abstract/Client.cs
@@ -56,7 +56,12 @@ namespace Netly.Abstract
         }
         public virtual void OnError(Action<Exception> callback)
         {
+            onErrorHandler += (_, exception) =>
+            {
+                MainThread.Add(() => callback?.Invoke(exception));
+            };
         }
+
         public virtual void OnOpen(Action callback)
         {
             onOpenHandler += (_, @null) =>
