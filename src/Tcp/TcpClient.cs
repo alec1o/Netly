74cbaa5701a79a536fb7850f61a0985d673806ea
Alecio Furanze <alecio@kezero.com>
2023-02-19 19:20:59 +0200

    re-create TcpClient.cs

--- src/Tcp/TcpClient.cs
+++ src/Tcp/TcpClient.cs
@@ -1,78 +1,45 @@
+﻿using Netly.Abstract;
+using Netly.Core;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Net.Sockets;
+using System.Text;
 using System.Threading;
-using Byter;
-using Netly.Core;
 
 namespace Netly
 {
-    /// <summary>
-    /// Netly: TcpClient
-    /// </summary>
-    public class TcpClient : IClient
+    public class TcpClient : Client
     {
-        public Host Host { get; private set; }
-        public bool IsOpened { get => Connected(); }
+        public TcpClient() { }
 
-        internal string uid;
-        private Socket _socket;
-
-        private bool _closed;
-        private bool _closing;
-        private bool _connecting;
-        private bool _serverMode;
-
-        private EventHandler<Socket> onModifyHandler;
-        private EventHandler<byte[]> onDataHandler;
-        private EventHandler<Exception> onErrorHandler;
-        private EventHandler onOpenHandler, onCloseHandler;
-        private EventHandler<(string name, byte[] data)> onEventHandler;
-
-
-        public TcpClient()
+        internal TcpClient(string uuid, Socket socket)
         {
-            _serverMode = false;
-        }
-
-        internal TcpClient(string id, Socket socket)
-        {
-            uid = id;
-            _socket = socket;
-            _serverMode = true;
+            UUID = uuid;
+            m_socket = socket;
             Host = new Host(socket.RemoteEndPoint);
-        }
+        } 
 
-
-        internal void InitServerMode()
+        public override void Open(Host host)
         {
-            onOpenHandler?.Invoke(null, null);
-            Receive();
-        }
+            if (IsOpened || m_connecting || m_closing || m_serverMode) return;
 
-        public void Open(Host host)
-        {
-            if (IsOpened || _connecting || _closing || _serverMode) return;
-
-            _connecting = true;
+            m_connecting = true;
 
             ThreadPool.QueueUserWorkItem(_ =>
             {
                 try
                 {
-                    _socket = new Socket(host.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                    m_socket = new Socket(host.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 
-                    _socket.NoDelay = true;
+                    m_socket.NoDelay = true;
 
-                    onModifyHandler?.Invoke(null, _socket);
+                    onModifyHandler?.Invoke(null, m_socket);
 
-                    _socket.Connect(host.EndPoint);
+                    m_socket.Connect(host.EndPoint);
 
                     Host = host;
 
-                    _closed = false;
+                    m_closed = false;
 
                     onOpenHandler?.Invoke(null, null);
 
@@ -83,87 +50,19 @@ namespace Netly
                     onErrorHandler?.Invoke(null, e);
                 }
 
-                _connecting = false;
+                m_connecting = false;
             });
         }
 
-        public void Close()
+        public override void Receive()
         {
-            if (!IsOpened || _connecting || _closing) return;
-
-            _closing = true;
-
-            _socket.Shutdown(SocketShutdown.Both);
+            int _length = 0;
+            byte[] _buffer = new byte[Package.MAX_SIZE];
+            Package _package = new Package();
 
             ThreadPool.QueueUserWorkItem(_ =>
             {
-                try
-                {
-                    _socket.Close();
-                    _socket.Dispose();
-                }
-                finally
-                {
-                    _socket = null;
-
-                    if (!_closed)
-                    {
-                        _closed = true;
-                        onCloseHandler?.Invoke(null, null);
-                    }
-                }
-
-                _closing = false;
-            });
-        }
-
-        public void ToData(byte[] data)
-        {
-            if (_closed) return;
-
-            byte[] buffer = Package.Create(ref data);
-
-            _socket.Send(buffer, 0, buffer.Length, SocketFlags.None);
-        }
-
-        public void ToEvent(string name, byte[] data)
-        {
-            ToData(MessageParser.Create(name, data));
-        }
-
-        public void ToData(string data)
-        {
-            this.ToData(NE.GetBytes(data, NE.Mode.UTF8));
-        }
-
-        public void ToEvent(string name, string data)
-        {
-            this.ToEvent(name, NE.GetBytes(data, NE.Mode.UTF8));
-        }
-
-        private bool Connected()
-        {
-            try
-            {
-                if (_socket == null || !_socket.Connected) return false;
-                const int timeout = 5000;
-                return !(_socket.Poll(timeout, SelectMode.SelectRead) && _socket.Available == 0);
-            }
-            catch
-            {
-                return false;
-            }
-        }
-
-        private void Receive()
-        {
-            int m_length = 0;
-            byte[] m_buffer = new byte[Package.MAX_SIZE];
-            Package m_package = new Package();
-
-            ThreadPool.QueueUserWorkItem(_ =>
-            {
-                m_package.Output((buffer) =>
+                _package.Output((buffer) =>
                 {
                     (string name, byte[] buffer) content = MessageParser.Verify(buffer);
 
@@ -173,90 +72,51 @@ namespace Netly
                         onEventHandler?.Invoke(null, (content.name, content.buffer));
                 });
 
-                while (!_closed)
+                while (m_socket != null)
                 {
                     try
                     {
-                        m_length = _socket.Receive(m_buffer, 0, m_buffer.Length, SocketFlags.None);
+                        _length = m_socket.Receive(_buffer, 0, _buffer.Length, SocketFlags.None);
 
-                        if (m_length <= 0)
+                        if (_length <= 0)
                         {
-                            if (IsOpened is false) break;
-                            continue;
+                            if (IsOpened is true) continue;
+                            break;
                         }
 
-                        byte[] buffer = new byte[m_length];
-                        Array.Copy(m_buffer, 0, buffer, 0, buffer.Length);
+                        byte[] buffer = new byte[_length];
+                        Array.Copy(_buffer, 0, buffer, 0, buffer.Length);
 
-                        m_package.Input(buffer);
+                        _package.Input(buffer);
                     }
                     catch
                     {
-                        if (m_length <= 0)
-                        {
-                            if (IsOpened is false) break;
-                            continue;
-                        }
+                        if (IsOpened is false) break;
                     }
                 }
 
-                m_package = null;
+                _package = null;
 
-                if (!_closing || !_closed)
+                if (!m_closing || !m_closed)
                 {
-                    _closed = true;
+                    m_closed = true;
                     onCloseHandler?.Invoke(null, null);
                 }
             });
-
         }
 
-        public void OnOpen(Action callback)
+        protected override bool IsConnected()
         {
-            onOpenHandler += (_, __) =>
+            try
             {
-                MainThread.Add(() => callback?.Invoke());
-            };
-        }
-
-        public void OnClose(Action callback)
-        {
-            onCloseHandler += (_, __) =>
+                if (m_socket == null || !m_socket.Connected) return false;
+                const int timeout = 5000;
+                return !(m_socket.Poll(timeout, SelectMode.SelectRead) && m_socket.Available == 0);
+            }
+            catch
             {
-                MainThread.Add(() => callback?.Invoke());
-            };
-        }
-
-        public void OnError(Action<Exception> callback)
-        {
-            onErrorHandler += (_, exception) =>
-            {
-                MainThread.Add(() => callback?.Invoke(exception));
-            };
-        }
-
-        public void OnData(Action<byte[]> callback)
-        {
-            onDataHandler += (_, buffer) =>
-            {
-                MainThread.Add(() => callback?.Invoke(buffer));
-            };
-        }
-
-        public void OnEvent(Action<string, byte[]> callback)
-        {
-            onEventHandler += (_, content) =>
-            {
-                MainThread.Add(() => callback?.Invoke(content.name, content.data));
-            };
-        }
-
-        public void OnModify(Action<Socket> callback)
-        {
-            onModifyHandler += (_, socket) =>
-            {
-                MainThread.Add(() => callback?.Invoke(socket));
-            };
+                return false;
+            }
         }
     }
 }
