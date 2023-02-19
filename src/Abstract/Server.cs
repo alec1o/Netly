7ad9ba2dbe9d2ae6e7574ca3650adfeed1b284ad
Alecio Furanze <alecio@kezero.com>
2023-02-19 22:09:30 +0200

    Create Server.cs

---
+++ src/Abstract/Server.cs
@@ -0,0 +1,10 @@
+﻿using Netly.Core;
+using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Netly.Abstract
+{
+    public abstract class Server<T>
+    {
