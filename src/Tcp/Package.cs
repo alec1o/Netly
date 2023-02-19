4afe3cfe4a3e951638d955be4d73f460929839e9
Alecio Furanze <alecio@kezero.com>
2023-02-19 16:19:00 +0200

    init Package.cs

--- src/Tcp/Package.cs
+++ src/Tcp/Package.cs
@@ -7,5 +7,15 @@ namespace Netly.Core
 {
     public class Package
     {
+        public static int MAX_SIZE = (1024 * 8);
+        private int count = 0;
+        private List<byte> bytes = new List<byte>();
+        private EventHandler<byte[]> onOutputHandler;
+
+
+        public void Output(Action<byte[]> callback)
+        {
+            onOutputHandler += (_, data) => callback?.Invoke(data);
+        }
     }
 }
