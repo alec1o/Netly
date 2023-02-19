924be86236ca06b15144c5ec49ce0593f766b2ac
Alecio Furanze <alecio@kezero.com>
2023-02-19 22:17:47 +0200

    Create TcpServers.cs

---
+++ src/Abstract/TcpServers.cs
@@ -0,0 +1,11 @@
+﻿using Netly.Core;
+using System;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Netly.Abstract
+{
+    public class TcpServers : Server<TcpClient>, IServer<TcpClient>
+    {
+    }
+}
