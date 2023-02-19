d24167975ed7f62df863b22b2cdabd2e73e5760a
Alecio Furanze <alecio@kezero.com>
2023-02-19 16:35:04 +0200

    Create PackageTest.cs

---
+++ test/PackageTest.cs
@@ -0,0 +1,42 @@
+﻿using Netly.Core;
+
+namespace test;
+
+public class PackageTest
+{
+    [Fact]
+    public void EndToEnd()
+    {
+        byte[] value1 = NE.GetBytes("HELLO");
+        byte[] value2 = NE.GetBytes("WORLD");
+
+        byte[] size1 = BitConverter.GetBytes(value1.Length);
+        byte[] size2 = BitConverter.GetBytes(value2.Length);
+
+        byte[] data1 = null;
+        byte[] data2 = null;
+
+        byte[] buffer = new List<byte[]> { size1, value1, size1 }.SelectMany(x => x).ToArray();
+
+        Package package = new Package();
+
+        package.Output((data) =>
+        {
+            if (data1 == null)
+            {
+                data1 = data;
+            }
+            else
+            {
+                data2 = data;
+            }
+        });
+
+        package.Input(buffer);
+        package.Input(value2);
+
+        Assert.Equal(value1, data1);
+        Assert.Equal(value2, data2);
+    }
+}
+
