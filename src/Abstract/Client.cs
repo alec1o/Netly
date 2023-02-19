d6fad917ef0c5aacfc94d823c70c17f32a05d784
Alecio Furanze <alecio@kezero.com>
2023-02-19 18:51:10 +0200

    Create Client.cs

---
+++ src/Abstract/Client.cs
@@ -0,0 +1,13 @@
+﻿using Netly.Core;
+using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Netly.Abstract
+{
+    public abstract class Client : IClient
+    {
+    }
+}
