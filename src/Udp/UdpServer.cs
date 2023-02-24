8448f5819c6d00c0dbf5293ee89559fee6d23404
Alecio Furanze <alecio@kezero.com>
2023-02-24 14:50:25 +0200

    re-create UdpServer

--- src/Udp/UdpServer.cs
+++ src/Udp/UdpServer.cs
@@ -1,426 +1,138 @@
-﻿using Netly;
+﻿using Netly.Abstract;
 using Netly.Core;
 using System;
-using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 
 namespace Netly
 {
-    /// <summary>
-    /// Netly: UdpServer
-    /// </summary>
-    public class UdpServer : IUdpServer
+    public class UdpServer : Server<UdpClient>, IServer<UdpClient>
     {
-        #region Var
+        public int Timeout { get; private set; }
 
-        /// <summary>
-        /// Endpoint
-        /// </summary>
-        public Host Host { get; private set; }
-
-        /// <summary>
-        /// Returns true if socket is connected
-        /// </summary>
-        public bool IsOpened { get => Connected(); }
-
-        /// <summary>
-        /// Returns list of UdpClient
-        /// </summary>
-        public List<UdpClient> Clients { get; private set; }
-
-        private Socket _socket;
-
-        private bool _tryOpen;
-        private bool _tryClose;
-        private bool _invokeClose;
-        private bool _opened;
-
-        private EventHandler _OnOpen;
-        private EventHandler _OnClose;
-        private EventHandler<Exception> _OnError;
-        private EventHandler<UdpClient> _OnEnter;
-        private EventHandler<UdpClient> _OnExit;
-        private EventHandler<(UdpClient client, byte[] data)> _OnData;
-        private EventHandler<(UdpClient client, string name, byte[] data)> _OnEvent;
-
-        private EventHandler<Socket> _OnModify;
-
-        #endregion
-
-
-        /// <summary>
-        /// Creating instance
-        /// </summary>
-        public UdpServer()
+        public override void Open(Host host, int timeout)
         {
-            Host = new Host(IPAddress.Any, 0);
-            Clients = new List<UdpClient>();
-            _socket = new Socket(Host.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
+            if (timeout < UdpClient.MIN_TIMEOUT) throw new ArgumentException("TIMEOUT is very low");
+            Timeout = timeout;
+            Open(host);
         }
 
-        #region Init
-
-        /// <summary>
-        ///  Use to open connection
-        /// </summary>
-        /// <param name="host"></param>
-        public void Open(Host host) => Open(host, 0);
-
-        /// <summary>
-        /// Use to open connection
-        /// </summary>
-        /// <param name="host">Endpoint</param>
-        /// <param name="backlog">Backlog</param>
-        public void Open(Host host, int backlog = 0)
+        public override void Open(Host host)
         {
-            if (IsOpened || _tryOpen || _tryClose) return;
+            if (IsOpened || m_tryOpen || m_tryClose) return;
 
-            _tryOpen = true;
+            m_tryOpen = true;
 
             ThreadPool.QueueUserWorkItem(_ =>
             {
                 try
                 {
-                    _socket = new Socket(host.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
+                    m_socket = new Socket(host.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
 
-                    _OnModify?.Invoke(this, _socket);
+                    onModifyHandler?.Invoke(null, m_socket);
 
-                    _socket.Bind(host.EndPoint);
+                    m_socket.Bind(host.EndPoint);
 
                     Host = host;
 
-                    _opened = true;
-                    _invokeClose = false;
+                    m_opened = true;
+                    m_invokeClose = false;
 
-                    _OnOpen?.Invoke(this, EventArgs.Empty);
+                    onOpenHandler?.Invoke(null, null);
 
-                    BeginAccept();
+                    AcceptOrReceive();
                 }
                 catch (Exception e)
                 {
-                    _OnError?.Invoke(this, e);
+                    onErrorHandler?.Invoke(null, e);
                 }
 
-                _tryOpen = false;
+                m_tryOpen = false;
             });
         }
 
-        /// <summary>
-        /// Use to close connection
-        /// </summary>
-        public void Close()
+        protected override void AcceptOrReceive()
         {
-            if (!IsOpened || _tryOpen || _tryClose) return;
-
-            _tryClose = true;
-
-            _socket.Shutdown(SocketShutdown.Both);
+            int length = 0;
+            byte[] buffer = new byte[Package.MAX_SIZE];
+            EndPoint endpoint = new IPEndPoint(IPAddress.Any, 0);
 
             ThreadPool.QueueUserWorkItem(_ =>
             {
-                try
-                {
-                    _socket.Close();
-                    _socket.Dispose();
-                }
-                finally
-                {
-                    _socket = null;
-
-                    foreach (UdpClient client in Clients.ToArray())
-                    {
-                        try
-                        {
-                            client?.Close();
-                        }
-                        catch { }
-                    }
-
-                    _opened = false;
-                    Clients.Clear();
-
-                    if (!_invokeClose)
-                    {
-                        _invokeClose = true;
-                        _OnClose?.Invoke(this, EventArgs.Empty);
-                    }
-                }
-
-                _tryClose = false;
-            });
-        }
-
-        private bool Connected()
-        {
-            if (_socket == null) return false;
-
-            return _opened;
-        }
-
-        private void BeginAccept()
-        {
-            try
-            {
-                byte[] buffer = new byte[1024 * 8];
-                int length = 0;
-                EndPoint endpoint = new IPEndPoint(IPAddress.Any, 0);
-
-                while (!_invokeClose)
+                while (IsOpened)
                 {
                     try
                     {
-                        length = _socket.ReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref endpoint);
+                        length = m_socket.ReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref endpoint);
+
+                        if (length <= 0) continue;
 
                         byte[] data = new byte[length];
-
-                        Buffer.BlockCopy(buffer, 0, data, 0, data.Length);
-
-                        IPEndPoint ipe = (IPEndPoint)endpoint;
-
-                        EndAccept(ipe.Address, ipe.Port, data);
+                        Array.Copy(buffer, 0, data, 0, data.Length);
+                        EndAccept(endpoint, data);
                     }
-                    catch
-                    {
-                        if (IsOpened)
-                        {
-                            continue;
-                        }
+                    catch { }
+                }
 
-                        break;
-                    }
-                }
-            }
-            catch
-            {
-                if (IsOpened)
-                {
-                    BeginAccept();
-                }
-            }
+                Console.WriteLine("Server closed");
+            });
         }
 
-        private void EndAccept(IPAddress address, int port, byte[] data)
+        private void EndAccept(EndPoint endpoint, byte[] buffer)
         {
-            // find udp client
-            foreach (UdpClient target in Clients.ToArray())
+            // find existed client
+            string endpointString = ((IPEndPoint)endpoint).ToString();
+
+            foreach (UdpClient target in Clients)
             {
-                if (target.Host.Port == port && target.Host.Address.ToString() == address.ToString())
+                if (target.Host.IPEndPoint.ToString() == endpointString)
                 {
-                    // add received data
-                    target.AddData(data);
-
-                    if (data.Length <= 0)
-                    {
-                        target.Close();
-                        Clients.Remove(target);
-                        _OnExit?.Invoke(this, target);
-                    }
-
+                    target.AddData(buffer);
                     return;
                 }
             }
 
             // create new client
-
-            UdpClient client = new UdpClient(Guid.NewGuid().ToString(), ref _socket, new Host(address, port));
-
-            // add this instance in list of all clients connected
-            Clients.Add(client);
+            UdpClient client = new UdpClient(Guid.NewGuid().ToString(), ref m_socket, new Host(endpoint));
+            AddOrRemoveClient(client, false);
 
             client.OnOpen(() =>
             {
-                _OnEnter?.Invoke(this, client);
+                onEnterHandler?.Invoke(null, client);
             });
 
             client.OnClose(() =>
             {
-                foreach (UdpClient target in Clients.ToArray())
-                {
-                    if (client.Id == target.Id)
-                    {
-                        try
-                        {
-                            Clients.Remove(target);
-                        }
-                        catch { }
-
-                        _OnExit?.Invoke(this, client);
-                    }
-                }
+                onExitHandler?.Invoke(null, AddOrRemoveClient(client, true));
             });
 
-            client.OnData((value) =>
+            client.OnData((data) =>
             {
-                _OnData?.Invoke(this, (client, value));
+                onDataHandler?.Invoke(null, (client, data));
             });
 
-            client.OnEvent((name, value) =>
+            client.OnEvent((name, data) =>
             {
-                _OnEvent?.Invoke(this, (client, name, value));
+                onEventHandler?.Invoke(null, (client, name, data));
             });
 
-            // init this instance for start work
+            client.Timeout = Timeout;
             client.InitServer();
-
-            // add received data
-            client.AddData(data);
+            client.AddData(buffer);
         }
 
-        /// <summary>
-        /// Sends raw data to all connected clients
-        /// </summary>
-        /// <param name="data">The date to be published</param>
-        public void BroadcastToData(byte[] data)
+        public override void OnClose(Action callback)
         {
-            if (!IsOpened || data == null) return;
-
-            foreach (UdpClient client in Clients.ToArray())
-            {
-                client.ToData(data);
-            }
+            Timeout = 0;
+            base.OnClose(callback);
         }
 
-        /// <summary>
-        /// Sends formatted "event" data to all connected clients
-        /// </summary>
-        /// <param name="name">Event name "subscription"</param>
-        /// <param name="data">The date to be published</param>
-        public void BroadcastToEvent(string name, byte[] data)
+        protected override bool IsConnected()
         {
-            if (!IsOpened || data == null) return;
+            if (m_socket == null) return false;
 
-            foreach (UdpClient client in Clients.ToArray())
-            {
-                client.ToEvent(name, data);
-            }
+            return m_opened;
         }
-
-        #endregion
-
-        /// <summary>
-        /// Is called, executes action before socket connect
-        /// </summary>
-        /// <param name="callback">action/callback</param>
-        public void OnModify(Action<Socket> callback)
-        {
-            _OnModify += (sender, socket) =>
-            {
-                MainThread.Add(() =>
-                {
-                    callback?.Invoke(socket);
-                });
-            };
-        }
-
-        #region Events
-
-        /// <summary>
-        ///  Execute the callback, when: the connection is opened
-        /// </summary>
-        /// <param name="callback">action/callback</param>
-        public void OnOpen(Action callback)
-        {
-            _OnOpen += (sender, args) =>
-            {
-                MainThread.Add(() =>
-                {
-                    callback?.Invoke();
-                });
-            };
-        }
-
-        /// <summary>
-        /// Execute the callback, when: the connection is closed
-        /// </summary>
-        /// <param name="callback">action/callback</param>
-        public void OnClose(Action callback)
-        {
-            _OnClose += (sender, args) =>
-            {
-                MainThread.Add(() =>
-                {
-                    callback?.Invoke();
-                });
-            };
-        }
-
-        /// <summary>
-        /// Execute the callback, when: the connection cannot be opened
-        /// </summary>
-        /// <param name="callback">action/callback</param>
-        public void OnError(Action<Exception> callback)
-        {
-            _OnError += (sender, exception) =>
-            {
-                MainThread.Add(() =>
-                {
-                    callback?.Invoke(exception);
-                });
-            };
-        }
-
-        /// <summary>
-        /// Execute the callback, when: a client connects
-        /// </summary>
-        /// <param name="callback">action/callback</param>
-        public void OnEnter(Action<UdpClient> callback)
-        {
-            _OnEnter += (sender, client) =>
-            {
-                MainThread.Add(() =>
-                {
-                    callback?.Invoke(client);
-                });
-            };
-        }
-
-        /// <summary>
-        /// Execute the callback, when: a client disconnects
-        /// </summary>
-        /// <param name="callback">action/callback</param>
-        public void OnExit(Action<UdpClient> callback)
-        {
-            _OnExit += (sender, client) =>
-            {
-                MainThread.Add(() =>
-                {
-                    callback?.Invoke(client);
-                });
-            };
-        }
-
-        /// <summary>
-        /// Execute the callback, when: the connection receives raw data
-        /// </summary>
-        /// <param name="callback">action/callback</param>
-        public void OnData(Action<UdpClient, byte[]> callback)
-        {
-            _OnData += (sender, value) =>
-            {
-                MainThread.Add(() =>
-                {
-                    callback?.Invoke(value.client, value.data);
-                });
-            };
-        }
-
-        /// <summary>
-        /// Execute the callback, when: the connection receives event data
-        /// </summary>
-        /// <param name="callback">action/callback</param>
-        public void OnEvent(Action<UdpClient, string, byte[]> callback)
-        {
-            _OnEvent += (sender, value) =>
-            {
-                MainThread.Add(() =>
-                {
-                    callback?.Invoke(value.client, value.name, value.data);
-                });
-            };
-        }
-
-        #endregion
     }
 }
