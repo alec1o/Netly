352d43e1a3e5ecc3820b11805a28ade076523d1e
Alecio Furanze <alecio@kezero.com>
2023-02-24 14:47:47 +0200

    dispose socket on close

--- src/Tcp/TcpClient.cs
+++ src/Tcp/TcpClient.cs
@@ -96,11 +96,14 @@ namespace Netly
                 }
 
                 _package = null;
+                m_socket?.Shutdown(SocketShutdown.Both);
 
                 if (!m_closing || !m_closed)
                 {
                     m_closed = true;
                     onCloseHandler?.Invoke(null, null);
+                    m_socket?.Dispose();
+                    m_socket = null;
                 }
             });
         }
