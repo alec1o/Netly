d2fcdc6af7ee85bdd329a1862c01b2690dab4e6d
Alecio Furanze <alecio@kezero.com>
2023-08-01 06:41:53 +0200

    ``improve`` update NE supporting ASCII, UTF7, UTF8, UTF16 (UNICODE), UTF32

--- src/Core/NE.cs
+++ src/Core/NE.cs
@@ -1,16 +1,48 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 
 namespace Netly.Core
 {
     /// <summary>
-    /// Netly: NE (Netly Encode). It is a module that redraws the (System.Text.Encoding) syntax
+    /// NE (Netly Encoding)
     /// </summary>
     public static class NE
     {
         /// <summary>
         /// Are the supported encoders method
         /// </summary>
-        public enum Mode { ASCII = 0, UTF8 = 1, UNICODE = 2 }
+        public enum Mode
+        {
+            /// <summary>
+            /// ASCII
+            /// </summary>
+            ASCII = 0,
+
+            /// <summary>
+            /// UTF-7
+            /// </summary>
+            UTF7 = 1,
+
+            /// <summary>
+            /// UTF-8
+            /// </summary>
+            UTF8 = 2,
+
+            /// <summary>
+            /// UTF-16
+            /// </summary>
+            UTF16 = 3,
+
+            /// <summary>
+            /// UTF-32
+            /// </summary>
+            UTF32 = 4,
+
+            /// <summary>
+            /// UNICODE is UTF-16
+            /// </summary>
+            UNICODE = 5,
+        }
 
         /// <summary>
         /// Is the default (generic) encoder used when encoding is not specified<br/>
@@ -18,42 +50,44 @@ namespace Netly.Core
         /// </summary>
         public static Mode Default { get; set; } = Mode.UTF8;
 
+
         /// <summary>
-        /// Convert value to string
+        /// Convert string to bytes (using NE.Default Mode)
         /// </summary>
         /// <param name="value">Value</param>
-        /// <returns>Return value converted to string</returns>
+        /// <returns></returns>
+        public static byte[] GetBytes(string value)
+        {
+            return GetBytes(value, Default);
+        }
+
+        /// <summary>
+        /// Convert bytes to string (using NE.Default Mode)
+        /// </summary>
+        /// <param name="value">Value</param>
+        /// <returns></returns>
         public static string GetString(byte[] value)
         {
             return GetString(value, Default);
         }
 
         /// <summary>
-        /// Convert value to string
+        /// Convert bytes to string
         /// </summary>
         /// <param name="value">Value</param>
-        /// <param name="encode">Encode method</param>
-        /// <returns>Return value converted to string</returns>
+        /// <param name="encode">Encoding protocol</param>
+        /// <returns></returns>
         public static string GetString(byte[] value, Mode encode)
         {
             switch (encode)
             {
-                case Mode.UTF8:
-                    {
-                        return Encoding.UTF8.GetString(value);
-                    }
-                case Mode.ASCII:
-                    {
-                        return Encoding.ASCII.GetString(value);
-                    }
-                case Mode.UNICODE:
-                    {
-                        return Encoding.Unicode.GetString(value);
-                    }
-                default:
-                    {
-                        return string.Empty;
-                    }
+                case Mode.ASCII: return Encoding.ASCII.GetString(value);
+                case Mode.UTF7: return Encoding.UTF7.GetString(value);
+                case Mode.UTF8: return Encoding.UTF8.GetString(value);
+                case Mode.UTF16: return Encoding.Unicode.GetString(value);
+                case Mode.UTF32: return Encoding.UTF32.GetString(value);
+                case Mode.UNICODE: return Encoding.Unicode.GetString(value);
+                default: return string.Empty;
             }
         }
 
@@ -61,38 +95,19 @@ namespace Netly.Core
         /// Convert value to bytes
         /// </summary>
         /// <param name="value">Value</param>
-        /// <returns>Return value converted to bytes</returns>
-        public static byte[] GetBytes(string value)
-        {
-            return GetBytes(value, Default);
-        }
-
-        /// <summary>
-        /// Convert value to bytes
-        /// </summary>
-        /// <param name="value">Value</param>
-        /// <param name="encode">Encode method</param>
-        /// <returns>Return value converted to bytes</returns>
+        /// <param name="encode">Encoding protocol</param>
+        /// <returns></returns>
         public static byte[] GetBytes(string value, Mode encode)
         {
             switch (encode)
             {
-                case Mode.UTF8:
-                    {
-                        return Encoding.UTF8.GetBytes(value);
-                    }
-                case Mode.ASCII:
-                    {
-                        return Encoding.ASCII.GetBytes(value);
-                    }
-                case Mode.UNICODE:
-                    {
-                        return Encoding.Unicode.GetBytes(value);
-                    }
-                default:
-                    {
-                        return new byte[0];
-                    }
+                case Mode.ASCII: return Encoding.ASCII.GetBytes(value);
+                case Mode.UTF7: return Encoding.UTF7.GetBytes(value);
+                case Mode.UTF8: return Encoding.UTF8.GetBytes(value);
+                case Mode.UTF16: return Encoding.Unicode.GetBytes(value);
+                case Mode.UTF32: return Encoding.UTF32.GetBytes(value);
+                case Mode.UNICODE: return Encoding.Unicode.GetBytes(value);
+                default: return Array.Empty<byte>();
             }
         }
     }
