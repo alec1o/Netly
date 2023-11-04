3058c9374985f5fea7fd02562f9ce4ec9af4c067
Alecio Furanze <alecio@kezero.com>
2023-11-04 06:19:50 +0200

    Create KeyValue.cs

---
+++ src/Core/KeyValue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Netly.Core
+{
+    public struct KeyValue<KeyType, ValueType>
+    {
+        public KeyType Key { get; internal set; }
+        public ValueType Value { get; internal set; }
+            
+        public KeyValue(KeyType key, ValueType value)
+        {
+            this.Key = key;
+            this.Value = value;
+        }
+    }
+}
