00c0bf324f31ef22d49b85636c746977241b3e0c
Alecio Furanze <alecio@kezero.com>
2023-11-04 06:44:51 +0200

    add xml docs on BuffeType.cs

--- src/Core/BufferType.cs
+++ src/Core/BufferType.cs
@@ -1,7 +1,18 @@
 ﻿namespace Netly.Core
 {
+    /// <summary>
+    /// Enum of Buffer Format Type
+    /// </summary>
     public enum BufferType
     {
+        /// <summary>
+        /// Binary Type
+        /// </summary>
         Binary = 0,
+        
+        /// <summary>
+        /// Text Type
+        /// </summary>
+        Text = 1,
     }
 }
