bad13fbe354d96fc645c996006e29926795f4e0d
Alecio Furanze <alecio@kezero.com>
2023-11-04 06:29:02 +0200

    add xml docs on KeyValue.cs

--- src/Core/KeyValue.cs
+++ src/Core/KeyValue.cs
@@ -1,9 +1,27 @@
 ﻿namespace Netly.Core
 {
+    /// <summary>
+    /// Generic Key-and-Value Implementation
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
     public struct KeyValue<TKey, TValue>
     {
+        /// <summary>
+        /// Key (name) 
+        /// </summary>
         public TKey Key { get; internal set; }
+        
+        /// <summary>
+        /// Value (data)
+        /// </summary>
         public TValue Value { get; internal set; }
+        
+        /// <summary>
+        /// Create instance of Generic Key-Value
+        /// </summary>
+        /// <param name="key">Key (Name)</param>
+        /// <param name="value">Value (Data)</param>
         public KeyValue(TKey key, TValue value)
         {
             this.Key = key;
