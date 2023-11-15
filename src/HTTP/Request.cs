70f3bdaef93b3d09eb98c3bd0af9b3ae72f0a85a
Alecio Furanze <alecio@kezero.com>
2023-11-15 11:46:11 +0200

    impl constructor on Request.cs

--- src/HTTP/Request.cs
+++ src/HTTP/Request.cs
@@ -1,7 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using Netly.Core;
+
 namespace Netly
 {
     public class Request
     {
+        public readonly HttpListenerRequest RawRequest;
+        public readonly KeyValueContainer Headers;
+        public readonly KeyValueContainer Queries;
+        public readonly Cookie[] Cookies;
+        public readonly HttpMethod Method;
+        public readonly string Url;
+        public readonly string Path;
+        public readonly Host LocalEndPoint;
+        public readonly Host RemoteEndPoint;
+        public readonly bool IsWebSocket;
+        public readonly bool IsLocalRequest;
+        public readonly bool IsEncrypted;
+
+        public Request(HttpListenerRequest httpListenerRequest)
+        {
+            RawRequest = httpListenerRequest;
+        }
 
     }
 }
