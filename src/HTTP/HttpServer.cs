f613e15814905e65afd2a3d3cc3127d2e2aca4fe
Alecio Furanze <alecio@kezero.com>
2023-11-15 19:19:46 +0200

    impl ``HttpServer`` -> ``OnOpen``

--- src/HTTP/HttpServer.cs
+++ src/HTTP/HttpServer.cs
@@ -4,6 +4,7 @@ namespace Netly
     public class HttpServer : IHttpServer
     {
         private HttpListener _listener;
+        private EventHandler<object> _onOpen, _onClose, _onError, _onPath, _onWebsocket;
         public bool IsOpen => _listener != null && _listener.IsListening;
         public Host Host { get; private set; } = new Host(IPAddress.Any, 0);
         public void Open(Host host)
@@ -13,6 +14,9 @@ namespace Netly
 
         public void OnOpen(Action callback)
         {
+            if (callback == null) return;
+
+            _onOpen += (_, __) => MainThread.Add(() => callback?.Invoke());
         }
 
         public void OnError(Action<Exception> callback)
