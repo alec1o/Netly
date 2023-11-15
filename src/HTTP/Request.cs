5ef764013464c6210b30c5b0b9596968019fa8a9
Alecio Furanze <alecio@kezero.com>
2023-11-15 11:47:07 +0200

    init constructors variables on Request.cs

--- src/HTTP/Request.cs
+++ src/HTTP/Request.cs
@@ -23,7 +23,77 @@ namespace Netly
         public Request(HttpListenerRequest httpListenerRequest)
         {
             RawRequest = httpListenerRequest;
+
+            #region Headers
+
+            Headers = new KeyValueContainer();
+
+            if (RawRequest.Headers.Count > 0)
+            {
+                foreach (var key in RawRequest.Headers.AllKeys)
+                {
+                    string value = RawRequest.Headers[key] ?? string.Empty;
+                    Headers.Add(key, value);
+                }
+            }
+
+            #endregion
+
+            #region Queries
+
+            Queries = new KeyValueContainer();
+
+            if (RawRequest.QueryString.Count > 0)
+            {
+                foreach (var key in RawRequest.QueryString.AllKeys)
+                {
+                    string value = RawRequest.QueryString[key] ?? string.Empty;
+                    Queries.Add(key, value);
+                }
+            }
+
+            #endregion
+
+            #region Cookies
+
+            var cookiesList = new List<Cookie>();
+
+            if (RawRequest.Cookies.Count > 0)
+            {
+                foreach (var cookie in RawRequest.Cookies)
+                {
+                    cookiesList.Add((Cookie)cookie);
+                }
+            }
+
+            Cookies = cookiesList.ToArray();
+
+            #endregion
+
+            Method = new HttpMethod(RawRequest.HttpMethod);
+            
+            Url = RawRequest.Url.AbsoluteUri;
+            
+            Path = RawRequest.Url.LocalPath;
+
+            LocalEndPoint = new Host(RawRequest.LocalEndPoint);
+            
+            RemoteEndPoint = new Host(RawRequest.RemoteEndPoint);
+
+            IsWebSocket = RawRequest.IsWebSocketRequest;
+
+            IsLocalRequest = RawRequest.IsLocal;
+
+            IsEncrypted = RawRequest.IsSecureConnection;
         }
 
+
+        public bool ComparePath(string path)
+        {
+            if (string.IsNullOrWhiteSpace(path)) return false;
+            if (string.IsNullOrWhiteSpace(Path)) return false;
+
+            return Path.Trim() == path.Trim();
+        }
     }
 }
