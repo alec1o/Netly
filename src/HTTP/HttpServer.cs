921cd5b1c238ffecea74cad2e3be2f9f87b8de71
Alecio Furanze <alecio@kezero.com>
2023-11-15 19:16:36 +0200

    Create HttpServer.cs

---
+++ src/HTTP/HttpServer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace Netly
+{
+    public class HttpServer : IHttpServer
+    {
+        private HttpListener _listener;
+        public bool IsOpen => _listener != null && _listener.IsListening;
+        public Host Host { get; private set; } = new Host(IPAddress.Any, 0);
+        public void Open(Host host)
+        {
+
+        }
+
+        public void OnOpen(Action callback)
+        {
+        }
+
+        public void OnError(Action<Exception> callback)
+        {
+        }
+
+        public void Close()
+        {
+
+        }
+
+        public void OnClose(Action callback)
+        {
+
+        }
+
+        public void On(string path, Action<Request, Response> callback)
+        {
+
+        }
+
+        public void OnWebsocket(string path, Action<Request, WebSocketClient> callback)
+        {
+
+        }
