4ed2fcfcad175ef276861991c148412b63a40803
Alecio Furanze <alecio@kezero.com>
2023-11-15 19:22:58 +0200

    impl ``HttpServer`` -> ``On``

--- src/HTTP/HttpServer.cs
+++ src/HTTP/HttpServer.cs
@@ -5,8 +5,30 @@ namespace Netly
     {
         private HttpListener _listener;
         private EventHandler<object> _onOpen, _onClose, _onError, _onPath, _onWebsocket;
+        private readonly List<(string url, bool isWebSocket)> _paths;
+
         public bool IsOpen => _listener != null && _listener.IsListening;
         public Host Host { get; private set; } = new Host(IPAddress.Any, 0);
+
+        public HttpServer()
+        {
+            _paths = new List<(string url, bool isWebSocket)>();
+        }
+
+        private struct PathContainer
+        {
+            public Request Request { get; private set; }
+            public Response Response { get; private set; }
+            public WebSocketClient WebSocket { get; private set; }
+
+            public PathContainer(Request request, Response response, WebSocketClient webSocketClient)
+            {
+                this.Request = request;
+                this.Response = response;
+                this.WebSocket = webSocketClient;
+            }
+        }
+
         public void Open(Host host)
         {
 
@@ -40,7 +62,19 @@ namespace Netly
 
         public void On(string path, Action<Request, Response> callback)
         {
+            if (string.IsNullOrWhiteSpace(path)) return;
 
+            _paths.Add((path.Trim(), false));
+
+            _onPath += (_, o) =>
+            {
+                var pathContainer = (PathContainer)o;
+
+                if (pathContainer.Request.ComparePath(path))
+                {
+                    MainThread.Add(() => callback?.Invoke(pathContainer.Request, pathContainer.Response));
+                }
+            };
         }
 
         public void OnWebsocket(string path, Action<Request, WebSocketClient> callback)
