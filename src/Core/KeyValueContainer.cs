4402dd896e879525018a30e14bd972bbc226db42
Alecio Furanze <alecio@kezero.com>
2023-11-15 11:39:42 +0200

    rename Header.cs to KeyValueContainer.cs

--- src/Core/KeyValueContainer.cs
+++ src/Core/KeyValueContainer.cs
@@ -4,66 +4,66 @@ using Netly.Core;
 namespace Netly
 {
     /// <summary>
-    /// Http Header Container
+    /// KeyValue Element Container
     /// </summary>
-    public class Headers
+    public class KeyValueContainer
     {
-        private readonly List<KeyValue<string, string>> _headers;
+        private readonly List<KeyValue<string, string>> _list;
         private readonly object _lock = new object();
 
         /// <summary>
-        /// Return all headers
+        /// Return all KV elements
         /// </summary>
-        public KeyValue<string, string>[] AllHeaders => GetAllHeaders();
+        public KeyValue<string, string>[] AllKeyValue => GetAllKeyValue();
         
         /// <summary>
-        /// Return the size (count) of all headers
+        /// Return the size (count) of all KV elements
         /// </summary>
         public int Count => GetLength();
         
         /// <summary>
-        /// Return the size (length) of all headers
+        /// Return the size (length) of all KV elements
         /// </summary>
         public int Length => GetLength();
 
         /// <summary>
         /// Create new instance
         /// </summary>
-        public Headers()
+        public KeyValueContainer()
         {
-            _headers = new List<KeyValue<string, string>>();
+            _list = new List<KeyValue<string, string>>();
         }
 
         
         /// <summary>
-        /// Return the amount of all headers
+        /// Return the amount of all KV element
         /// </summary>
         /// <returns></returns>
         public int GetLength()
         {
             lock (_lock)
             {
-                return _headers.Count;
+                return _list.Count;
             }
         }
 
         /// <summary>
-        /// Return all headers
+        /// Return all KV element
         /// </summary>
         /// <returns></returns>
-        public KeyValue<string, string>[] GetAllHeaders()
+        public KeyValue<string, string>[] GetAllKeyValue()
         {
             lock (_lock)
             {
-                return _headers.ToArray();
+                return _list.ToArray();
             }
         }
 
         /// <summary>
-        /// Add a header at container
+        /// Add a KV element in container
         /// </summary>
-        /// <param name="name">Header name (Key)</param>
-        /// <param name="value">Header data (Value)</param>
+        /// <param name="name">name (Key)</param>
+        /// <param name="value">data (Value)</param>
         public void Add(string name, string value)
         {
             if (string.IsNullOrWhiteSpace(name)) return;
@@ -71,25 +71,25 @@ namespace Netly
             lock (_lock)
             {
                 Remove(name);
-                _headers.Add(new KeyValue<string, string>(name, value));
+                _list.Add(new KeyValue<string, string>(name, value));
             }
         }
 
         /// <summary>
-        /// Remove a header if exist
+        /// Remove a KV element if exist
         /// </summary>
-        /// <param name="name">Header name (Key)</param>
+        /// <param name="name">name (Key)</param>
         public void Remove(string name)
         {
             if (!string.IsNullOrEmpty(Get(name)))
             {
                 lock (_lock)
                 {
-                    foreach (var kv in _headers)
+                    foreach (var kv in _list)
                     {
                         if (kv.Key.ToLower() == name.ToLower())
                         {
-                            _headers.Remove(kv);
+                            _list.Remove(kv);
                         }
                     }
                 }
@@ -97,17 +97,17 @@ namespace Netly
         }
 
         /// <summary>
-        /// Return value (data) of some header. <br/>
+        /// Return value (data) of some KV element. <br/>
         /// </summary>
-        /// <param name="name">Header name (Key)</param>
-        /// <returns>It return value (data) of a header name (key). <br/> Warning it return Empty (null) when data not found or header value (data) is Empty (White Space)</returns>
+        /// <param name="name">name (Key)</param>
+        /// <returns>It return value (data) of a name (key). <br/> Warning it return Empty (null) when data not found or KV element value (data) is Empty (White Space)</returns>
         public string Get(string name)
         {
             if (string.IsNullOrWhiteSpace(name)) return string.Empty;
 
             lock (_lock)
             {
-                foreach (var kv in _headers)
+                foreach (var kv in _list)
                 {
                     if (kv.Key.ToLower() == name.ToLower())
                     {
