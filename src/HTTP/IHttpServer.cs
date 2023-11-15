5662ed7e02a570b9206d0aea0992c1379b3445ab
Alecio Furanze <alecio@kezero.com>
2023-11-15 11:40:05 +0200

    Create IHttpServer.cs

---
+++ src/HTTP/IHttpServer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Netly.Core;
+
+namespace Netly
+{
+    internal interface IHttpServer
+    {
+        bool IsOpen { get; }
+        Host Host { get; }
+        
+        void Open(Host host);
+        void OnOpen(Action callback);
+
+        void OnError(Action<Exception> exception);
+        
+        void Close();
+        void OnClose(Action callback);
+
+        void On(string path, Action<Request, Response> callback);
+        void OnWebsocket(string path, Action<Request, WebSocketClient> callback);
+    }
+}
