879d280b88a6c0351b07b208719faa9abcee4a43
Alecio Furanze <alecio@kezero.com>
2023-11-15 11:51:44 +0200

    impl Response.cs constructor

--- src/HTTP/Response.cs
+++ src/HTTP/Response.cs
@@ -2,5 +2,13 @@ namespace Netly
 {
     public class Response
     {
+        public readonly HttpListenerResponse RawResponse;
 
+        public Response(HttpListenerResponse httpListenerResponse)
+        {
+            RawResponse = httpListenerResponse;
+        }
+
+
+    }
 }
