2183d541625917e0b1a4a5ca8e1bf145783165aa
Alecio Furanze <alecio@kezero.com>
2024-01-27 02:05:47 +0200

    add unity icon on docs header

--- docs/netly-theme/public/main.js
+++ docs/netly-theme/public/main.js
@@ -4,6 +4,11 @@ export default {
 			icon: 'github',
 			href: 'https://github.com/alec1o/Netly',
 			title: 'GitHub'
+		},
+		{
+			icon: 'unity',
+			href: 'https://u3d.as/2RUX',
+			title: 'Unity Asset Store'
 		}
 	]
 }
