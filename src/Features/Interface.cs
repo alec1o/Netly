1c646cd7a124b15aadd61e93211e5c1c7f107a4c
Alecio Furanze <alecio@kezero.com>
2024-01-28 08:35:26 +0200

    add ``IMap`` and ``IHttpServer`` on Interface.cs

--- src/Features/Interface.cs
+++ src/Features/Interface.cs
@@ -12,22 +12,22 @@ namespace Netly.Features
         /// Enctype type
         /// </summary>
         Enctype Enctype { get; }
-        
+
         /// <summary>
         /// Text buffer
         /// </summary>
         string Text { get; }
-        
+
         /// <summary>
         /// Binary buffer
         /// </summary>
         byte[] Binary { get; }
-        
+
         /// <summary>
         /// Get value from Enctype content (return string)
         /// </summary>
         Dictionary<string, string> TextQueries { get; }
-        
+
         /// <summary>
         /// Get value from Enctype content (return bytes)
         /// </summary>
@@ -45,62 +45,62 @@ namespace Netly.Features
         /// Request Headers
         /// </summary>
         Dictionary<string, string> Headers { get; }
-        
+
         /// <summary>
         /// Request Queries
         /// </summary>
         Dictionary<string, string> Queries { get; }
-        
+
         /// <summary>
         /// Request Params
         /// </summary>
         Dictionary<string, string> Params { get; }
-        
+
         /// <summary>
         /// Request Cookies
         /// </summary>
         Cookie[] Cookies { get; }
-        
+
         /// <summary>
         /// Request Http Method
         /// </summary>
         HttpMethod Method { get; }
-        
+
         /// <summary>
         /// Request Url
         /// </summary>
         string Url { get; }
-        
+
         /// <summary>
         /// Request Path
         /// </summary>
         string Path { get; }
-        
+
         /// <summary>
         /// Request (local end point)
         /// </summary>
         Host LocalEndPoint { get; }
-        
+
         /// <summary>
         /// Request (remote end point)
         /// </summary>
         Host RemoteEndPoint { get; }
-        
+
         /// <summary>
         /// Return true if request is websocket
         /// </summary>
         bool IsWebSocket { get; }
-        
+
         /// <summary>
         /// Return true if request is same host
         /// </summary>
         bool IsLocalRequest { get; }
-        
+
         /// <summary>
         /// Return true if connection is encrypted e.g SSL or TLS protocol
         /// </summary>
         bool IsEncrypted { get; }
-        
+
         /// <summary>
         /// Request Body
         /// </summary>
@@ -175,5 +175,125 @@ namespace Netly.Features
         /// <returns>true if callback added successful</returns>
         /// <returns></returns>
         bool Add(string path, Func<IRequest, IResponse, bool> middleware);
+    }
+
+    internal interface IMap
+    {
+        /// <summary>
+        /// Handle WebSocket from Path
+        /// </summary>
+        /// <param name="path">Request Path</param>
+        /// <param name="callback">Response Callback</param>
+        void WebSocket(string path, Action<IRequest, IWebsocketClient> callback);
+        
+        /// <summary>
+        /// Handle All Http Method from Path
+        /// </summary>
+        /// <param name="path">Request Path</param>
+        /// <param name="callback">Response Callback</param>
+        void All(string path, Action<IRequest, IResponse> callback);
+        
+        /// <summary>
+        /// Handle (Get) Http Method
+        /// </summary>
+        /// <param name="path">Request Path</param>
+        /// <param name="callback">Response Callback</param>
+        void Get(string path, Action<IRequest, IResponse> callback);
+        
+        /// <summary>
+        /// Handle (Put) Http Method
+        /// </summary>
+        /// <param name="path">Request Path</param>
+        /// <param name="callback">Response Callback</param>
+        void Put(string path, Action<IRequest, IResponse> callback);
+        
+        /// <summary>
+        /// Handle (Head) Http Method
+        /// </summary>
+        /// <param name="path">Request Path</param>
+        /// <param name="callback">Response Callback</param>
+        void Head(string path, Action<IRequest, IResponse> callback);
+        
+        /// <summary>
+        /// Handle (Post) Http Method
+        /// </summary>
+        /// <param name="path">Request Path</param>
+        /// <param name="callback">Response Callback</param>
+        void Post(string path, Action<IRequest, IResponse> callback);
+        
+        /// <summary>
+        /// Handle (Patch) Http Method
+        /// </summary>
+        /// <param name="path">Request Path</param>
+        /// <param name="callback">Response Callback</param>
+        void Patch(string path, Action<IRequest, IResponse> callback);
+        
+        /// <summary>
+        /// Handle (Delete) Http Method
+        /// </summary>
+        /// <param name="path">Request Path</param>
+        /// <param name="callback">Response Callback</param>
+        void Delete(string path, Action<IRequest, IResponse> callback);
+        
+        /// <summary>
+        /// Handle (Trace) Http Method
+        /// </summary>
+        /// <param name="path">Request Path</param>
+        /// <param name="callback">Response Callback</param>
+        void Trace(string path, Action<IRequest, IResponse> callback);
+        
+        /// <summary>
+        /// Handle (Options) Http Method
+        /// </summary>
+        /// <param name="path">Request Path</param>
+        /// <param name="callback">Response Callback</param>
+        void Options(string path, Action<IRequest, IResponse> callback);
+    }
+
+    internal interface IHttpServer
+    {
+        /// <summary>
+        /// Server Uri
+        /// </summary>
+        Uri Host { get; }
+        
+        /// <summary>
+        /// Middleware Object
+        /// </summary>
+        IMiddleware Middleware { get; }
+        
+        /// <summary>
+        /// Map Object
+        /// </summary>
+        IMap Map { get; }
+        
+        /// <summary>
+        /// Handle error, before connection Opened
+        /// </summary>
+        /// <param name="callback">Callback</param>
+        void OnError(Action<Exception> callback);
+        
+        /// <summary>
+        /// Open Server Connection
+        /// </summary>
+        /// <param name="host">Server Uri</param>
+        void Open(Uri host);
+        
+        /// <summary>
+        /// Handle Connection Opened
+        /// </summary>
+        /// <param name="callback">Callback</param>
+        void OnOpen(Action callback);
+        
+        /// <summary>
+        /// Close Server Connection
+        /// </summary>
+        void Close();
+        
+        /// <summary>
+        /// Handle Connection Close
+        /// </summary>
+        /// <param name="callback">Callback</param>
+        void OnClose(Action callback);
     }
 }
