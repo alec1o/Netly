380143451f2e621422833d179884db6141eb52d5
Alecio Furanze <alecio@kezero.com>
2024-01-30 09:36:11 +0200

    Create HTTP Container (``HTTP.cs``)

---
+++ src/Features/HTTP.cs
@@ -0,0 +1,29 @@
+﻿namespace Netly.Features
+{
+    /// <summary>
+    /// HTTP Container
+    /// </summary>
+    public partial class HTTP
+    {
+        /// <summary>
+        /// HTTP Client
+        /// </summary>
+        public partial class Client
+        {
+        }
+
+        /// <summary>
+        /// HTTP Server
+        /// </summary>
+        public partial class Server
+        {
+        }
+
+        /// <summary>
+        /// WebSocket Client
+        /// </summary>
+        public partial class WebSocket
+        {
+        }
+    }
+}
