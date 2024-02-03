b83d17486e76a034bb38404ac137e7f4105d2b2f
Alecio Furanze <alecio@kezero.com>
2024-02-03 10:44:38 +0200

    Create HTTP.Server.IMiddlewareContainer.cs

---
+++ src/Features/Interfaces/HTTP.Server.IMiddlewareContainer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Netly.Interfaces
+{
+    public partial class HTTP
+    {
+        public partial class Server
+        {
+            public interface IMiddlewareContainer
+            {
+                string Path { get; }
+                Func<IRequest, IResponse, bool> Callback { get; }
+            }
+        }
+    }
+}
