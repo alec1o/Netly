2fa5c0b28db12f5bf50b47a530bbece803ad52bb
Alecio Furanze <alecio@kezero.com>
2024-02-07 07:48:28 +0200

    Create Netly.cs

---
+++ src/Netly/Netly.cs
@@ -0,0 +1,32 @@
+﻿namespace Netly
+{
+    /// <summary>
+    /// Netly
+    /// </summary>
+    public static partial class Netly
+    {
+        /// <summary>
+        /// Netly Name
+        /// </summary>
+        public const string Name = "Netly";
+        
+        /// <summary>
+        /// Netly Version
+        /// </summary>
+        public const string Version = "4.0.0";
+        
+        /// <summary>
+        /// Netly supported protocols
+        /// </summary>
+        public static readonly string[] Protocols = new[] { "TCP", "UDP", "HTTP", "WebSocket" };
+        
+        /// <summary>
+        /// Netly Git repository
+        /// </summary>
+        public const string GitRepository = "https://github.com/alec1o/Netly";
+        
+        /// <summary>
+        /// Netly logger hub
+        /// </summary>
+        public static readonly ILogger Logger;
+}
