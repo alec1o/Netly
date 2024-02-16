4d86182b24e4da56260dff0a731819559cbcb636
Alecio Furanze <alecio@kezero.com>
2024-02-16 16:19:50 +0200

    Create HTTP.Client.ITo.cs

---
+++ src/HTTP/Interfaces/HTTP.Client.ITo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Netly.Core;
+
+namespace Netly
+{
+    public static partial class HTTP
+    {
+        public partial class Client
+        {
+            /// <summary>
+            ///     HTTP.Client action creator container
+            /// </summary>
+            public interface ITo
+            {
+                /// <summary>
+                ///     Create http fetch <br/>
+                ///     + Only if(IsOpened==false)
+                /// </summary>
+                /// <param name="method">Http method</param>
+                /// <param name="url">Fetch url</param>
+                /// <param name="body">Request body</param>
+                void Fetch(string method, string url, byte[] body = null);
+
+                /// <summary>
+                ///     Create http fetch <br/>
+                ///     + Only if(IsOpened==false)
+                /// </summary>
+                /// <param name="method">Http method</param>
+                /// <param name="url">Fetch url</param>
+                /// <param name="body">Request body</param>
+                /// <param name="encode">Body encoding algorithm</param>
+                void Fetch(string method, string url, string body = null,NE.Encoding encode = NE.Encoding.UTF8);
+            }
+        }
+    }
+}
