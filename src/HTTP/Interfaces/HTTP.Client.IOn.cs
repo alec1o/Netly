2b253c9d6b37019092dfa9edc058484dd5c3e18b
Alecio Furanze <alecio@kezero.com>
2024-02-16 16:19:58 +0200

    Create HTTP.Client.IOn.cs

---
+++ src/HTTP/Interfaces/HTTP.Client.IOn.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Netly
+{
+    public static partial class HTTP
+    {
+        public partial class Client
+        {
+            /// <summary>
+            ///     HTTP.Client callbacks container
+            /// </summary>
+            public interface IOn : IOn<System.Net.Http.HttpClient>
+            {
+                /// <summary>
+                ///     Handle fetch response
+                /// </summary>
+                /// <param name="callback">Callback</param>
+                void Fetch(Action<IRequest> callback);
+            }
+        }
+    }
+}
