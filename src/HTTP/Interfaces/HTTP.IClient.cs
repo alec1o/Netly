92654374decd21e07fe9b8252a223147a13bf741
Alecio Furanze <alecio@kezero.com>
2024-02-16 16:20:14 +0200

    Create HTTP.IClient.cs

---
+++ src/HTTP/Interfaces/HTTP.IClient.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using Netly.Core;
+
+namespace Netly
+{
+    public static partial class HTTP
+    {
+        internal interface IClient
+        {
+            Dictionary<string, string> Headers { get; }
+            Dictionary<string, string> Queries { get; }
+            
+            int Timeout { get; set; }
+            bool IsOpened { get; }
+            
+            Client.IOn On { get; }
+            Client.ITo To { get; }
+        }
+    }
+}
