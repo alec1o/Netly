eed4a4db64a74272a9e03af8c9858d7f9fefd87e
Alecio Furanze <alecio@kezero.com>
2024-02-28 07:07:22 +0200

    Create TCP.IClient.cs

---
+++ src/Tcp/Interfaces/TCP.IClient.cs
@@ -0,0 +1,18 @@
+﻿using Netly.Core;
+
+namespace Netly
+{
+    public static partial class TCP
+    {
+        internal interface IClient
+        {
+            string Id { get; }
+            Host Host { get; }
+            bool IsOpened { get; }
+            bool IsFraming { get; }
+            bool IsEncrypted { get; }
+            Client.ITo To { get; }
+            Client.IOn On { get; }
+        }
+    }
+}
