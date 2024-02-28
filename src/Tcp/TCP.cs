0557676afaa7dccf16db7dec7c3f1c4ab5fda9d6
Alecio Furanze <alecio@kezero.com>
2024-02-28 07:06:27 +0200

    Create TCP.cs

---
+++ src/Tcp/TCP.cs
@@ -0,0 +1,22 @@
+﻿namespace Netly
+{
+    /// <summary>
+    /// TCP Container
+    /// </summary>
+    public static partial class TCP
+    {
+        /// <summary>
+        /// TCP Server
+        /// </summary>
+        public partial class Server
+        {
+        }
+
+        /// <summary>
+        /// TCP Client
+        /// </summary>
+        public partial class Client
+        {
+        }
+    }
+}
