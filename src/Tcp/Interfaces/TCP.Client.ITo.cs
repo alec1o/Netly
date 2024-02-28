9a307d420e64f95218eb16854ea48096c083febb
Alecio Furanze <alecio@kezero.com>
2024-02-28 07:07:05 +0200

    Create TCP.Client.ITo.cs

---
+++ src/Tcp/Interfaces/TCP.Client.ITo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Sockets;
+using Netly.Core;
+
+namespace Netly
+{
+    public static partial class TCP
+    {
+        public partial class Client
+        {
+            public interface ITo
+            {
+                void Open(Host host);
+                void Close();
+                void Data(byte[] data);
+                void Encryption(bool enable);
+                void Data(string data, NE.Encoding encoding = NE.Encoding.UTF8);
+                void Event(string name, byte[] data);
+                void Event(string name, string data, NE.Encoding encoding = NE.Encoding.UTF8);
+            }
+        }
+    }
+}
