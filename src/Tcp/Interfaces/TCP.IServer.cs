4d5f11a0322063c7e5e8ea479eecd0d201be9373
Alecio Furanze <alecio@kezero.com>
2024-03-01 18:11:55 +0200

    Create TCP.IServer.cs

---
+++ src/Tcp/Interfaces/TCP.IServer.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using Netly.Core;
+
+namespace Netly
+{
+    public static partial class TCP
+    {
+        internal interface IServer
+        {
+            string Id { get; }
+            Host Host { get; }
+            bool IsOpened { get; }
+            bool IsFraming { get; }
+            X509Certificate2 Certificate { get; }
+            SslProtocols EncryptionProtocol { get; }
+            bool IsEncrypted { get; }
+            Server.ITo To { get; }
+            Server.IOn On { get; }
+        }
+    }
+}
