d63c905fc3a78de41e0232769a0599fb7cfb9eec
Alecio Furanze <alecio@kezero.com>
2024-03-12 21:36:37 +0200

    Create UDP.Client.IOn.cs

---
+++ src/Udp/Interfaces/UDP.Client.IOn.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace Netly
+{
+    public static partial class UDP
+    {
+        public partial class Client
+        {
+            public interface IOn : IOn<Socket>
+            {
+                void Data(Action<byte[]> callback);
+                void Event(Action<string, byte[]> callback);
+            }
+        }
+    }
+}
