228f7d5c7b890f5fa92fa0d3ac099f3c5c8e2b4d
Alecio Furanze <alecio@kezero.com>
2024-03-12 21:23:15 +0200

    move Enctype from Core to HTTP.Enctype

--- src/Core/Enctype.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Netly.Core
-{
-    public enum Enctype
-    {
-        UrlEncoded = 0,
-        Multipart = 1,
-        PlainText = 2,
-    }
-}
---
+++ src/HTTP/Partials/HTTP.Enctype.cs
@@ -0,0 +1,12 @@
+﻿namespace Netly
+{
+    public static partial class HTTP
+    {
+        public enum Enctype
+        {
+            UrlEncoded = 0,
+            Multipart = 1,
+            PlainText = 2,
+        }
+    }
+}
