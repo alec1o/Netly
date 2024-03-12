7b2e9d44bfd2441625a8e8786d1e03af272ed021
Alecio Furanze <alecio@kezero.com>
2024-03-12 21:37:33 +0200

    Create UDP.cs

---
+++ src/Udp/UDP.cs
@@ -0,0 +1,22 @@
+﻿namespace Netly
+{
+    /// <summary>
+    /// UDP Container
+    /// </summary>
+    public static partial class UDP
+    {
+        /// <summary>
+        /// UDP Server
+        /// </summary>
+        public partial class Server
+        {
+        }
+
+        /// <summary>
+        /// UDP Client
+        /// </summary>
+        public partial class Client
+        {
+        }
+    }
+}
