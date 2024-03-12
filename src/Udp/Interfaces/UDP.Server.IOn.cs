f9230a11f24b1221a6bc7c5c78b67e16ea5db7fc
Alecio Furanze <alecio@kezero.com>
2024-03-12 21:37:19 +0200

    Create UDP.Server.IOn.cs

---
+++ src/Udp/Interfaces/UDP.Server.IOn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace Netly
+{
+    public static partial class UDP
+    {
+        public partial class Server
+        {
+            public interface IOn : IOn<Socket>
+            {
+                void Accept(Action<IClient> callback);
+            }
+        }
+    }
+}
