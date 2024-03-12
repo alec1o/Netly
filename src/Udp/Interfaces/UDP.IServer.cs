8c0951206c728203ec913a84aa8ada7be0ff9e96
Alecio Furanze <alecio@kezero.com>
2024-03-12 21:37:24 +0200

    Create UDP.IServer.cs

---
+++ src/Udp/Interfaces/UDP.IServer.cs
@@ -0,0 +1,18 @@
+﻿using Netly.Core;
+
+namespace Netly
+{
+    public static partial class UDP
+    {
+        internal interface IServer
+        {
+            string Id { get; }
+            Host Host { get; }
+            bool IsOpened { get; }
+            bool UseConnection { get; }
+            Server.ITo To { get; }
+            Server.IOn On { get; }
+            IClient[] Clients { get; }
+        }
+    }
+}
