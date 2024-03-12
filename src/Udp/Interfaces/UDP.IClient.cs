51ac448b7df5da4a58275bd6be990be65eab1d0d
Alecio Furanze <alecio@kezero.com>
2024-03-12 21:37:01 +0200

    Create UDP.IClient.cs

---
+++ src/Udp/Interfaces/UDP.IClient.cs
@@ -0,0 +1,17 @@
+﻿using Netly.Core;
+
+namespace Netly
+{
+    public static partial class UDP
+    {
+        public interface IClient
+        {
+            string Id { get; }
+            Host Host { get; }
+            bool IsOpened { get; }
+            bool UseConnection { get; }
+            Client.ITo To { get; }
+            Client.IOn On { get; }
+        }
+    }
+}
