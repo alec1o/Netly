8f7e7c087169fe16de87c64d2d7d8296cca99f9b
Alecio Furanze <alecio@kezero.com>
2024-03-12 21:36:53 +0200

    Create UDP.Client.ITo.cs

---
+++ src/Udp/Interfaces/UDP.Client.ITo.cs
@@ -0,0 +1,20 @@
+﻿using Netly.Core;
+
+namespace Netly
+{
+    public static partial class UDP
+    {
+        public partial class Client
+        {
+            public interface ITo
+            {
+                void Open(Host host);
+                void Close();
+                void Data(byte[] data);
+                void Data(string data, NE.Encoding encoding = NE.Encoding.UTF8);
+                void Event(string name, byte[] data);
+                void Event(string name, string data, NE.Encoding encoding = NE.Encoding.UTF8);
+            }
+        }
+    }
+}
