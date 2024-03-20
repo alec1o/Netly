7ac17abfe89a96e8e307ec5395fb22360c52d628
Alecio Furanze <alecio@kezero.com>
2024-03-20 18:57:29 +0200

    Create UDP.Server.To.cs

---
+++ src/Udp/Partials/UDP.Server.To.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using Netly.Core;
+
+namespace Netly
+{
+    public partial class UDP
+    {
+        public partial class Server
+        {
+            public class _To : ITo
+            {
+                private readonly Server _server;
+                public bool IsOpened { get; set; }
+                public Host Host { get; set; }
+                public List<Client> Clients { get; set; }
+
+                public _To(Server server)
+                {
+                    _server = server;
+                    IsOpened = false;
+                    Host = new Host(IPAddress.Any, 0);
+                    Clients = new List<Client>();
+                }
+
+                public void Open(Host host)
+                {
+                    throw new System.NotImplementedException();
+                }
+
+                public void Close()
+                {
+                    throw new System.NotImplementedException();
+                }
+            }
+        }
+    }
+}
