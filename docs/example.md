ef4e64fd6147fb733760d1b6b165eca806217103
Alecio Furanze <alecio@kezero.com>
2024-04-20 00:33:31 +0200

    update NetlyFx, add nuget in page header

---
+++ docs/example.md
@@ -0,0 +1,30 @@
+# Netly Examples
+
+## HTTP
+[!TODO] Fetch API
+[!TODO] Listen API Route
+[!TODO] Serve Static Content
+[!TODO] WebSocket Echo
+[!TODO] WebSocket Auth Example
+
+## TCP
+[!TODO] Echo 
+[!TODO] Ping-Pong (loop)
+[!TODO] Login Example
+[!TODO] SSL/TLS Configuration
+
+## UDP
+[!TODO] Echo
+[!TODO] Ping-Pong (loop)
+[!TODO] Login Example
+
+## UDP
+[!TODO] Echo
+[!TODO] Ping-Pong (loop)
+[!TODO] Login Example
+
+## RUDP (Reliable UDP)
+[!TODO] Echo
+[!TODO] Ping-Pong (loop)
+[!TODO] Login Example
+[!TODO] Connection Configuration
--- docs/templates/NetlyFx/public/main.js
+++ docs/templates/NetlyFx/public/main.js
@@ -1,14 +1,19 @@
 export default {
-	iconLinks: [
-		{
-			icon: 'github',
-			href: 'https://github.com/alec1o/Netly',
-			title: 'GitHub'
-		},
-		{
-			icon: 'unity',
-			href: 'https://u3d.as/2RUX',
-			title: 'Unity Asset Store'
-		}
-	]
+    iconLinks: [
+        {
+            icon: 'github',
+            href: 'https://github.com/alec1o/Netly',
+            title: 'GitHub'
+        },
+        {
+            icon: 'unity',
+            href: 'https://u3d.as/2RUX',
+            title: 'Unity Asset Store'
+        },
+        {
+            icon: 'app-indicator',
+            href: 'https://www.nuget.org/packages/Netly',
+            title: 'Nuget Package Manager'
+        }
+    ]
 }
