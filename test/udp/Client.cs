774ea84e6fe159717c0471f96f51a8ff92dce797
Alecio Furanze <alecio@kezero.com>
2024-04-20 13:11:36 +0200

    impl udp send data test

--- test/udp/Client.cs
+++ test/udp/Client.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using System.Net.Sockets;
 using Netly;
 using Netly.Core;
@@ -104,7 +105,7 @@ public class Client
              * WARNING:
              * windows allow open udp connection at 0 port.
              * this test is disabled for run in windows because will fail.
-            */
+             */
             await client.To.Open(new Host("127.0.0.1", 0));
             await client.To.Close();
         }
@@ -172,10 +173,96 @@ public class Client
         }
     }
 
-    [Fact(Skip = "TODO: wait for server")]
-    public void SendData()
+    [Fact]
+    public async void SendData()
     {
-        throw new NotImplementedException();
+        // server
+        bool sConnected = false;
+        bool sError = false;
+        bool sClosed = false;
+        bool sAccept = false;
+        // client
+        bool cConnected = false;
+        bool cError = false;
+        bool cClosed = false;
+
+        string sentData1 = Guid.NewGuid().ToString();
+        string sentData2 = Guid.NewGuid().ToString();
+        string recvData1 = string.Empty;
+        string recvData2 = string.Empty;
+
+        UDP.Server server = new();
+
+        server.On.Open(() => sConnected = true);
+        server.On.Close(() => sClosed = true);
+        server.On.Error(e => sError = true);
+        server.On.Accept((client) =>
+        {
+            int count = 0;
+            client.On.Open(() => sAccept = true);
+
+            client.On.Data((data) =>
+            {
+                count++;
+
+                switch (count)
+                {
+                    case 1:
+                        recvData1 = NE.GetString(data);
+                        break;
+                    case 2:
+                        recvData2 = NE.GetString(data);
+                        break;
+                    default:
+                        Assert.Fail(new NotSupportedException().ToString());
+                        break;
+                }
+            });
+        });
+
+        // connect in random port
+        await server.To.Open(new Host(IPAddress.Loopback, 0));
+
+        Assert.True(sConnected);
+        Assert.False(sError);
+        Assert.False(sClosed);
+
+        // connect a client
+        UDP.Client client = new UDP.Client();
+
+        client.On.Open(() => cConnected = true);
+        client.On.Close(() => cClosed = true);
+        client.On.Error((e) => cError = true);
+
+        await client.To.Open(server.Host);
+
+        Assert.True(cConnected);
+        Assert.False(cError);
+        Assert.False(cClosed);
+
+        client.To.Data(sentData1);
+        await Task.Delay(50); // prevent unreceived data and inverse data order
+        client.To.Data(sentData2);
+        await Task.Delay(50); // prevent unreceived data and inverse data order
+
+
+        // server receive client connection
+        Assert.True(sAccept);
+
+        Assert.Equal(sentData1, recvData1);
+        //    ^^^^^^ server receive data successful
+
+        Assert.Equal(sentData2, recvData2);
+        //    ^^^^^^ server receive data successful
+
+        Assert.True(server.IsOpened);
+        Assert.True(client.IsOpened);
+
+        await server.To.Close();
+        await client.To.Close();
+
+        Assert.False(server.IsOpened);
+        Assert.False(client.IsOpened);
     }
 
     [Fact(Skip = "TODO: wait for server")]
