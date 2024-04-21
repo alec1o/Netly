d4a4328daa67377ce26df8c9538f241493328f10
Alecio Furanze <alecio@kezero.com>
2024-04-21 10:26:09 +0200

    Create IRUDP.ServerTo.cs

---
+++ src/rudp/interfaces/IRUDP.ServerTo.cs
@@ -0,0 +1,69 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using Netly.Core;
+
+namespace Netly.Interfaces
+{
+    public static partial class IRUDP
+    {
+        /// <summary>
+        ///     RUDP Server actions container (interface)
+        /// </summary>
+        public interface ServerTo
+        {
+            /// <summary>
+            ///     Use to open listening connection
+            /// </summary>
+            /// <param name="host">Host (local endpoint)</param>
+            /// <returns></returns>
+            Task Open(Host host);
+
+            /// <summary>
+            ///     Use to close listening connection
+            /// </summary>
+            /// <returns></returns>
+            Task Close();
+
+            /// <summary>
+            ///     Use to send raw data to all connected clients
+            /// </summary>
+            /// <param name="data">data - bytes</param>
+            void DataBroadcast(byte[] data);
+
+            /// <summary>
+            ///     Use to send raw data to all connected clients
+            /// </summary>
+            /// <param name="data">Data - string</param>
+            void DataBroadcast(string data);
+            
+            /// <summary>
+            ///     Use to send raw data to all connected clients
+            /// </summary>
+            /// <param name="data">Data - string</param>
+            /// <param name="encoding">Data encoding method</param>
+            void DataBroadcast(string data, Encoding encoding);
+
+            /// <summary>
+            ///     Use to send event (netly event) to all connected clients
+            /// </summary>
+            /// <param name="name">Event name</param>
+            /// <param name="data">Event data - bytes</param>
+            void EventBroadcast(string name, byte[] data);
+
+            /// <summary>
+            ///     Use to send event (netly event) to all connected clients
+            /// </summary>
+            /// <param name="name">Event name</param>
+            /// <param name="data">Event data - string</param>
+            void EventBroadcast(string name, string data);
+            
+            /// <summary>
+            ///     Use to send event (netly event) to all connected clients
+            /// </summary>
+            /// <param name="name">Event name</param>
+            /// <param name="data">Event data - string</param>
+            /// <param name="encoding">Event data encoding method</param>
+            void EventBroadcast(string name, string data, Encoding encoding);
+        }
+    }
+}
