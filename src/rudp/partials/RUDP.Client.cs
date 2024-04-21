0687583ef276f4b0c883bcba6a61216dc18b3125
Alecio Furanze <alecio@kezero.com>
2024-04-21 10:48:59 +0200

    Create RUDP.Client.cs

---
+++ src/rudp/partials/RUDP.Client.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Netly.Core;
+using Netly.Interfaces;
+
+namespace Netly
+{
+    public static partial class RUDP
+    {
+        public partial class Client : IRUDP.Client
+        {
+            private readonly ClientOn _on;
+            private readonly ClientTo _to;
+
+            public Client()
+            {
+                Id = Guid.NewGuid().ToString();
+                _on = new ClientOn();
+                _to = new ClientTo(this);
+            }
+
+            public bool IsOpened => _to.IsOpened;
+            public Host Host => _to.Host;
+            public IRUDP.ClientTo To => _to;
+            public IRUDP.ClientOn On => _on;
+            public string Id { get; }
+        }
+    }
+}
