781982dacc61d36080cb86cf326d8ee9d855cc73
Alecio Furanze <alecio@kezero.com>
2024-04-21 10:27:24 +0200

    Create RUDP.ServerOn.cs

---
+++ src/rudp/partials/RUDP.ServerOn.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net.Sockets;
+using Netly.Core;
+using Netly.Interfaces;
+
+namespace Netly
+{
+    public static partial class RUDP
+    {
+        public partial class Server
+        {
+            private class ServerOn : IRUDP.ServerOn
+            {
+                public EventHandler<IRUDP.Client> OnAccept;
+                public EventHandler OnClose;
+                public EventHandler<Exception> OnError;
+                public EventHandler<Socket> OnModify;
+                public EventHandler OnOpen;
+
+                public void Open(Action callback)
+                {
+                    OnOpen += (@object, @event) => MainThread.Add(() => callback?.Invoke());
+                }
+
+                public void Error(Action<Exception> callback)
+                {
+                    OnError += (@object, @event) => MainThread.Add(() => callback?.Invoke(@event));
+                }
+
+                public void Close(Action callback)
+                {
+                    OnClose += (@object, @event) => MainThread.Add(() => callback?.Invoke());
+                }
+
+                public void Modify(Action<Socket> callback)
+                {
+                    OnModify += (@object, @event) => MainThread.Add(() => callback?.Invoke(@event));
+                }
+
+                public void Accept(Action<IRUDP.Client> callback)
+                {
+                    OnAccept += (@object, @event) => MainThread.Add(() => callback?.Invoke(@event));
+                }
+            }
+        }
+    }
+}
