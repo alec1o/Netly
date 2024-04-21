ea803fefc4e74f4ba837ea88ed58046ae778d8f9
Alecio Furanze <alecio@kezero.com>
2024-04-21 10:26:45 +0200

    Create RUDP.cs

---
+++ src/rudp/RUDP.cs
@@ -0,0 +1,22 @@
+﻿namespace Netly
+{
+    /// <summary>
+    ///     RUDP Implementation
+    /// </summary>
+    public static partial class RUDP
+    {
+        /// <summary>
+        ///     RUDP Server implementation
+        /// </summary>
+        public partial class Server
+        {
+        }
+
+        /// <summary>
+        ///     RUDP Client implementation
+        /// </summary>
+        public partial class Client
+        {
+        }
+    }
+}
