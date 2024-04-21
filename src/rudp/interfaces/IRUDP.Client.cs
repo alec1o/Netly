9fb5b60836cf76d48f9f894ba894ce3b2777284a
Alecio Furanze <alecio@kezero.com>
2024-04-21 10:24:00 +0200

    Create IRUDP.Client.cs

---
+++ src/rudp/interfaces/IRUDP.Client.cs
@@ -0,0 +1,38 @@
+﻿using Netly.Core;
+
+namespace Netly.Interfaces
+{
+    public static partial class IRUDP
+    {
+        /// <summary>
+        ///     RUDP Client instance (interface)
+        /// </summary>
+        public interface Client
+        {
+            /// <summary>
+            ///     Client ID (readonly)
+            /// </summary>
+            string Id { get; }
+
+            /// <summary>
+            ///     Remote Host
+            /// </summary>
+            Host Host { get; }
+
+            /// <summary>
+            ///     Is Opened? (return true if connected)
+            /// </summary>
+            bool IsOpened { get; }
+
+            /// <summary>
+            ///     Actions container
+            /// </summary>
+            ClientTo To { get; }
+
+            /// <summary>
+            ///     Callbacks container
+            /// </summary>
+            ClientOn On { get; }
+        }
+    }
+}
