bc28a744f088551fd63c8519cc943eb42053fa2c
Alecio Furanze <alecio@kezero.com>
2024-04-21 10:40:41 +0200

    Create RUDP.Server.cs

---
+++ src/rudp/partials/RUDP.Server.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using Netly.Core;
+using Netly.Interfaces;
+
+namespace Netly
+{
+    public static partial class RUDP
+    {
+        public partial class Server : IRUDP.Server
+        {
+            private readonly ServerOn _on;
+            private readonly ServerTo _to;
+
+            public Server()
+            {
+                Id = Guid.NewGuid().ToString();
+                _on = new ServerOn();
+                _to = new ServerTo(this);
+            }
+
+            public string Id { get; }
+            public Host Host => _to.Host;
+            public bool IsOpened => _to.IsOpened;
+            public IRUDP.ServerTo To => _to;
+            public IRUDP.ServerOn On => _on;
+            public IRUDP.Client[] Clients => _to.Clients.Select(x => (IRUDP.Client)x).ToArray();
+        }
+    }
+}
