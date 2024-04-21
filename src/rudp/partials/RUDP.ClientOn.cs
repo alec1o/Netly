e15da4da26d4f762f7445c191eccb19d53b52fce
Alecio Furanze <alecio@kezero.com>
2024-04-21 10:27:10 +0200

    Create RUDP.ClientOn.cs

---
+++ src/rudp/partials/RUDP.ClientOn.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net.Sockets;
+using Netly.Core;
+using Netly.Interfaces;
+
+namespace Netly
+{
+    public static partial class RUDP
+    {
+        public partial class Client
+        {
+            private class ClientOn : IRUDP.ClientOn
+            {
+                public EventHandler OnClose;
+                public EventHandler<byte[]> OnData;
+                public EventHandler<Exception> OnError;
+                public EventHandler<(string name, byte[] buffer)> OnEvent;
+                public EventHandler<Socket> OnModify;
+                public EventHandler OnOpen;
+
+                public void Open(Action callback)
+                {
+                    OnOpen += (@object, @event) => MainThread.Add(() => callback?.Invoke());
+                }
+
+                public void Error(Action<Exception> callback)
+                {
+                    OnError += (@object, @event) => MainThread.Add(() => callback?.Invoke(@event));
+                }
+
+                public void Close(Action callback)
+                {
+                    OnClose += (@object, e) => MainThread.Add(() => callback?.Invoke());
+                }
+
+                public void Modify(Action<Socket> callback)
+                {
+                    OnModify += (@object, e) => MainThread.Add(() => callback?.Invoke(e));
+                }
+
+                public void Data(Action<byte[]> callback)
+                {
+                    OnData += (@object, e) => MainThread.Add(() => callback?.Invoke(e));
+                }
+
+                public void Event(Action<string, byte[]> callback)
+                {
+                    OnEvent += (@object, e) => MainThread.Add(() => callback?.Invoke(e.name, e.buffer));
+                }
+            }
+        }
+    }
+}
