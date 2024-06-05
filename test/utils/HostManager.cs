b4fa8fb50b90a3b3f6774bb6cca13cde7337b8d2
Alecio Furanze (Ale) <alecio@kezero.com>
2024-06-05 19:39:33 +0200

    HostManager: prevent thread error with lock()

--- test/utils/HostManager.cs
+++ test/utils/HostManager.cs
@@ -3,12 +3,16 @@ using System.Net;
 public static class HostManager
 {
     private const int Initial = 1024;
-    private const int Skip = 2;
+    private const int Skip = 3;
     private static int _index = Initial;
+    private static readonly object Locker = new();
 
     public static Host GenerateLocalHost()
     {
-        _index += Skip;
-        return new Host(IPAddress.Loopback, _index);
+        lock (Locker)
+        {
+            _index += Skip;
+            return new Host(IPAddress.Loopback, _index);
+        }
     }
 }
