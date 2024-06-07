ab113d5dbeec6e125da880637c373c706516991b
Alecio Furanze (Ale) <alecio@kezero.com>
2024-06-07 12:42:09 +0200

    Create FixUdp.OpenAndClose.cs

---
+++ test/tcp/FixUdp.OpenAndClose.cs
@@ -0,0 +1,125 @@
+using System.Net;
+
+public partial class FixTcp(ITestOutputHelper output)
+{
+    [Fact]
+    public void OpenAndClose()
+    {
+        var host = HostManager.GenerateLocalHost();
+        
+        Server();
+
+        async Task Client()
+        {
+            TCP.Client client = new();
+
+            bool isOpen = false, isClose = false, isError = false, isModify = false;
+
+            client.On.Open(() => isOpen = true);
+            client.On.Close(() => isClose = true);
+            client.On.Error(_ => isError = true);
+            client.On.Modify(_ => isModify = true);
+
+            {
+                Assert.False(client.IsOpened);
+                Assert.False(isOpen);
+                Assert.False(isClose);
+                Assert.False(isError);
+                Assert.False(isModify);
+            }
+
+            await client.To.Open(host);
+
+            Thread.Sleep(millisecondsTimeout: 10);
+            {
+                Assert.True(client.IsOpened);
+                Assert.True(isModify);
+                Assert.True(isOpen);
+                Assert.False(isClose);
+                Assert.False(isError);
+            }
+
+            await client.To.Close();
+
+            Thread.Sleep(millisecondsTimeout: 10);
+            {
+                Assert.False(client.IsOpened);
+                Assert.True(isModify);
+                Assert.True(isOpen);
+                Assert.True(isClose);
+                Assert.False(isError);
+            }
+
+            await client.To.Open(new Host(IPAddress.Any, 0));
+            {
+                Assert.False(client.IsOpened);
+                Assert.True(isModify);
+                Assert.True(isOpen);
+                Assert.True(isClose);
+                Assert.True(isError);
+            }
+
+            await client.To.Close();
+        }
+
+        async void Server()
+        {
+            TCP.Server server = new();
+
+            bool isOpen = false, isClose = false, isError = false, isModify = false;
+
+            server.On.Open(() => isOpen = true);
+            server.On.Close(() => isClose = true);
+            server.On.Modify(_ => isModify = true);
+            server.On.Error(exception =>
+            {
+                isError = true;
+                output.WriteLine(exception.ToString());
+            });
+
+            {
+                Assert.False(server.IsOpened);
+                Assert.False(isOpen);
+                Assert.False(isClose);
+                Assert.False(isError);
+                Assert.False(isModify);
+            }
+
+            await server.To.Open(host);
+
+            Thread.Sleep(millisecondsTimeout: 10);
+            {
+                Assert.True(server.IsOpened);
+                Assert.True(isModify);
+                Assert.True(isOpen);
+                Assert.False(isClose);
+                Assert.False(isError);
+            }
+
+            await Client();
+
+            await server.To.Close();
+
+            Thread.Sleep(millisecondsTimeout: 10);
+            {
+                Assert.False(server.IsOpened);
+                Assert.True(isModify);
+                Assert.True(isOpen);
+                Assert.True(isClose);
+                Assert.False(isError);
+            }
+
+            // Cannot assign requested address
+            await server.To.Open(new Host("1.1.1.1", 0));
+            {
+                Assert.False(server.IsOpened);
+                Assert.True(isModify);
+                Assert.True(isOpen);
+                Assert.True(isClose);
+                Assert.True(isError);
+            }
+
+            await server.To.Close();
+        }
+    }
+}
