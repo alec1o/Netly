04928616e1f6a19ad5f76370d2dd30bd627fad11
Alecio Furanze (Ale) <alecio@kezero.com>
2024-06-18 13:07:05 +0200

    HTTP.ClientResponse->implement missed features

--- src/http/partials/Client/HTTP.ClientResponse.cs
+++ src/http/partials/Client/HTTP.ClientResponse.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
+using System.Web;
 using Netly.Interfaces;
 
 namespace Netly
@@ -14,87 +15,56 @@ namespace Netly
             public ClientResponse(ref HttpResponseMessage response, ref HttpClient client)
             {
                 // native response
-                {
-                    NativeResponse = response;
-                }   
-                
+                NativeResponse = response;
+
                 // native client
-                {
-                    NativeClient = client;
-                }
-                
+                NativeClient = client;
+
                 // add headers
+                Headers = new Dictionary<string, string>();
+                response.Headers.ToList().ForEach(x =>
                 {
-                    Headers = new Dictionary<string, string>();
-
-                    foreach (var header in response.Headers)
-                    {
-                        string value = String.Empty;
-
-                        header.Value.ToList().ForEach(x => value += x);
-
-                        Headers.Add(header.Key, value);
-                    }
-                }
+                    var value = string.Empty;
+                    x.Value.ToList().ForEach(y => value += y);
+                    Headers.Add(x.Key, value);
+                });
 
                 // encoding
-                {
-                    // TODO: Get encoding from response header or set UTF8 as default
-                    Encoding = Encoding.UTF8;
-                }
+                Encoding = GetEncodingFromHeader();
 
                 // queries
+                Queries = new Dictionary<string, string>();
+                if (!string.IsNullOrWhiteSpace(response.RequestMessage.RequestUri.Query))
                 {
-                    // TODO: impl query
-                    Queries = new Dictionary<string, string>();
+                    var builder = new UriBuilder(response.RequestMessage.RequestUri);
+                    var collection = HttpUtility.ParseQueryString(builder.Query);
+                    foreach (var key in collection.AllKeys) Queries.Add(key, collection[key]);
                 }
 
                 // method
-                {
-                    Method = response.RequestMessage.Method;
-                }
+                Method = response.RequestMessage.Method;
 
                 // enctype
-                {
-                    // TODO: get enctype from header or None
-                    Enctype = Enctype.None;
-                }
+                Enctype = GetEnctypeFromHeader();
 
                 // Url
-                {
-                    // TODO: impl url
-                    // Url = response.RequestMessage.RequestUri;
-                    Url = string.Empty;
-                }
+                Url = response.RequestMessage.RequestUri.AbsoluteUri;
 
                 // path
-                {
-                    // TODO: iml path
-                    // Path = response.RequestMessage.RequestUri;
-                    Path = string.Empty + "";
-                }
+                Path = response.RequestMessage.RequestUri.LocalPath;
 
                 // is local
-                {
-                    IsLocalRequest = response.RequestMessage.RequestUri.IsLoopback;
-                }
+                IsLocalRequest = response.RequestMessage.RequestUri.IsLoopback;
 
                 // is encrypted
-                {
-                    IsEncrypted =
-                        response.RequestMessage.RequestUri.Scheme.Equals("HTTPS", StringComparison.OrdinalIgnoreCase);
-                }
+                IsEncrypted =
+                    response.RequestMessage.RequestUri.Scheme.Equals("HTTPS", StringComparison.OrdinalIgnoreCase);
 
                 // body
-                {
-                    byte[] buffer = response.Content.ReadAsByteArrayAsync().Result;
-                    Body = new Body(buffer, Enctype, this.Encoding);
-                }
+                Body = new Body(response.Content.ReadAsByteArrayAsync().Result, Enctype, Encoding);
 
                 // status
-                {
-                    Status = (int)response.StatusCode;
-                }
+                Status = (int)response.StatusCode;
             }
 
             public HttpResponseMessage NativeResponse { get; }
@@ -110,6 +80,40 @@ namespace Netly
             public bool IsEncrypted { get; }
             public IHTTP.Body Body { get; }
             public int Status { get; }
+
+            private Encoding GetEncodingFromHeader()
+            {
+                var comparisonType = StringComparison.InvariantCultureIgnoreCase;
+                var value = Headers.FirstOrDefault(x => x.Key.Equals("Content-Type", comparisonType));
+                var key = (value.Value ?? string.Empty).ToUpper();
+
+                if (string.IsNullOrWhiteSpace(key)) return Encoding.UTF8;
+
+                // generic
+                if (key.Contains("UTF-8")) return Encoding.UTF8;
+                if (key.Contains("ISO-8859-1")) return Encoding.GetEncoding("ISO-8859-1");
+                if (key.Contains("ASCII")) return Encoding.ASCII;
+                if (key.Contains("UTF-16")) return Encoding.Unicode;
+                if (key.Contains("UTF-32")) return Encoding.UTF32;
+                
+                // https://en.wikipedia.org/wiki/Character_encodings_in_HTML
+                return Encoding.UTF8;
+            }
+
+            private Enctype GetEnctypeFromHeader()
+            {
+                var comparisonType = StringComparison.InvariantCultureIgnoreCase;
+                var value = Headers.FirstOrDefault(x => x.Key.Equals("Content-Type", comparisonType));
+                var key = (value.Value ?? string.Empty).ToUpper();
+                
+                if (string.IsNullOrWhiteSpace(key)) return Enctype.None;
+                
+                if (key.Contains("application/x-www-form-urlencoded")) return Enctype.UrlEncoded;
+                if (key.Contains("multipart/form-data")) return Enctype.Multipart;
+                if (key.Contains("text/plain")) return Enctype.PlainText;
+                
+                return Enctype.None;
+            }
         }
     }
 }
