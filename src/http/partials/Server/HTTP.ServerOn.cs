128a938677d43009d177db65d549d5626a969a0e
Alecio Furanze (Ale) <alecio@kezero.com>
2024-06-27 11:49:16 +0200

    HTTP.ServerOn -> remove server scope

--- src/http/partials/Server/HTTP.ServerOn.cs
+++ src/http/partials/Server/HTTP.ServerOn.cs
@@ -7,34 +7,31 @@ namespace Netly
 {
     public partial class HTTP
     {
-        public partial class Server
+        internal class ServerOn : IHTTP.ServerOn
         {
-            internal class ServerOn : IHTTP.ServerOn
+            public EventHandler OnClose { get; private set; }
+            public EventHandler<Exception> OnError { get; private set; }
+            public EventHandler<HttpListener> OnModify { get; private set; }
+            public EventHandler OnOpen { get; private set; }
+
+            public void Open(Action callback)
             {
-                public EventHandler OnClose { get; private set; }
-                public EventHandler<Exception> OnError { get; private set; }
-                public EventHandler<HttpListener> OnModify { get; private set; }
-                public EventHandler OnOpen { get; private set; }
+                OnOpen += (@object, @event) => Env.MainThread.Add(() => callback?.Invoke());
+            }
 
-                public void Open(Action callback)
-                {
-                    OnOpen += (@object, @event) => Env.MainThread.Add(() => callback?.Invoke());
-                }
+            public void Error(Action<Exception> callback)
+            {
+                OnError += (@object, @event) => Env.MainThread.Add(() => callback?.Invoke(@event));
+            }
 
-                public void Error(Action<Exception> callback)
-                {
-                    OnError += (@object, @event) => Env.MainThread.Add(() => callback?.Invoke(@event));
-                }
+            public void Close(Action callback)
+            {
+                OnClose += (@object, @event) => Env.MainThread.Add(() => callback?.Invoke());
+            }
 
-                public void Close(Action callback)
-                {
-                    OnClose += (@object, @event) => Env.MainThread.Add(() => callback?.Invoke());
-                }
-
-                public void Modify(Action<HttpListener> callback)
-                {
-                    OnModify += (@object, @event) => Env.MainThread.Add(() => callback?.Invoke(@event));
-                }
+            public void Modify(Action<HttpListener> callback)
+            {
+                OnModify += (@object, @event) => Env.MainThread.Add(() => callback?.Invoke(@event));
             }
         }
     }
