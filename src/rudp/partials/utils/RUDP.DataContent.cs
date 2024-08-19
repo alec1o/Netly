2b1a37bd1a664c5bd5a51a31004648016004abe3
Alecio Furanze (Ale) <alecio@kezero.com>
2024-08-20 01:46:02 +0200

    Create RUDP.DataContent.cs

---
+++ src/rudp/partials/utils/RUDP.DataContent.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Netly
+{
+    public static partial class RUDP
+    {
+        private class DataContent
+        {
+            public readonly uint Id;
+            public readonly byte[] Data;
+            public readonly Host Host;
+            public DateTime TimeoutAt { get; private set; }
+
+            public DataContent(uint id, byte[] data, Host host)
+            {
+                Id = id;
+                Data = data;
+                Host = host;
+                UpdateTimeout();
+            }
+
+            public void UpdateTimeout()
+            {
+            }
+        }
+    }
+}
