ee57c17954af3c0e6c40a1c8f529cd575e0a2c56
Alecio Furanze (Ale) <alecio@kezero.com>
2024-08-20 03:46:27 +0200

    RUDP.Connection implementing Open Connection

--- src/rudp/partials/utils/RUDP.Connection.cs
+++ src/rudp/partials/utils/RUDP.Connection.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
+using Byter;
 
 namespace Netly
 {
@@ -9,6 +11,12 @@ namespace Netly
     {
         internal sealed class Connection
         {
+            private const int
+                HandshakeDataPrefix = -1024 * (int)Math.PI * 2, //# -6144
+                HandshakeData1 /**/ = -2048 * (int)Math.PI * 4, //# -24576
+                HandshakeData2 /**/ = -4096 * (int)Math.PI * 8; //# -98304
+
+            public readonly List<int> HandshakeDataQueue = new List<int>();
             public readonly string Id;
             public readonly bool IsServer;
             public readonly Channel MyChannel;
@@ -29,7 +37,7 @@ namespace Netly
                 };
             }
 
-            public bool IsOpened { get; }
+            public bool IsOpened { get; private set; }
             public bool IsConnecting { get; private set; }
             public Action OnOpen { private get; set; }
             public Action OnClose { private get; set; }
@@ -42,8 +50,16 @@ namespace Netly
 
             private void OnRawDataHandler(byte[] data, MessageType messageType)
             {
-                throw new NotImplementedException();
+                var primitive = new Primitive(data);
+
+                var prefix = primitive.Get.Int();
+                var content = primitive.Get.Int();
+
+                if (primitive.IsValid)
+                    if (prefix == HandshakeDataPrefix && IsConnecting)
+                        HandshakeDataQueue.Add(content);
             }
+
 
             private void SendRaw(byte[] bytes)
             {
@@ -60,7 +76,9 @@ namespace Netly
 
             public Task Open()
             {
+                IsOpened = false;
                 IsConnecting = true;
+                HandshakeDataQueue.Clear();
 
                 _ = Task.Run(() =>
                 {
@@ -74,7 +92,65 @@ namespace Netly
                     }
                 });
 
-                throw new NotImplementedException();
+                var connect = new Task(() =>
+                {
+                    var timeoutAt = DateTime.UtcNow.AddMilliseconds(HandshakeTimeout);
+                    var success = false;
+                    var failMessage = $"Connection closed, Handshake Timeout: {HandshakeTimeout}ms";
+
+                    if (!IsServer)
+                    {
+                        var primitive = new Primitive();
+
+                        // send 1st handshake data
+                        {
+                            primitive.Add.Int(HandshakeDataPrefix);
+                            primitive.Add.Int(HandshakeData1);
+
+                            var data = primitive.GetBytes();
+
+                            Send(ref data, MessageType.Reliable);
+                        }
+
+                        primitive.Reset();
+
+                        // send 2nd handshake data
+                        {
+                            primitive.Add.Int(HandshakeDataPrefix);
+                            primitive.Add.Int(HandshakeData2);
+
+                            var data = primitive.GetBytes();
+
+                            Send(ref data, MessageType.Reliable);
+                        }
+                    }
+
+                    while (timeoutAt > DateTime.UtcNow)
+                        if (HandshakeDataQueue.Count == 2)
+                        {
+                            if (HandshakeDataQueue[0] == HandshakeData1 && HandshakeDataQueue[1] == HandshakeData2)
+                                success = true;
+
+                            failMessage = "Invalid Handshake Method";
+                            break;
+                        }
+
+                    if (success)
+                    {
+                        IsOpened = true;
+                        IsConnecting = false;
+                        OnOpen();
+                    }
+                    else
+                    {
+                        IsConnecting = false;
+                        IsOpened = false;
+
+                        OnOpenFail(failMessage);
+                    }
+                });
+
+                return Task.Run(() => connect.Start());
             }
 
             public Task Close()
