1d02973fd51c763a7a7ba4f9ebe3238bd24be974
Alecio Furanze (Ale) <alecio@kezero.com>
2024-09-07 00:49:00 +0200

    docs. rename tutorials page to docs page

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -74,7 +74,7 @@ const config: Config = {
                     type: "docSidebar",
                     sidebarId: "tutorialSidebar",
                     position: "left",
-                    label: "Tutorial",
+                    label: "Docs",
                 },
                 { to: "/blog", label: "Blog", position: "left" },
                 {
