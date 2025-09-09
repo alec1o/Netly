1e67648c5a2b3414c24f75b2f79b8da9b200b922
Alecio Furanze (Ale) <alecio@kezero.com>
2025-09-09 04:26:02 +0200

    upgrade NFraming test

--- test/Netly/NFramingTest.cs
+++ test/Netly/NFramingTest.cs
@@ -7,19 +7,17 @@ public class NFramingTest
         // initialize logger
         NetlyEnvironment.Logger.On(output.WriteLine);
         NetlyEnvironment.Logger.On((Exception exception) => output.WriteLine(exception.ToString()));
-
-        // create demo message
-        MyMessage = "Hello World!"u8.ToArray();
-
-        // generate message header
-        MyHeader = NFraming.Create(MyMessage.LongLength);
     }
-
-    private readonly byte[] MyMessage, MyHeader;
 
     [Fact]
     public void Start()
     {
+        byte[]
+            // create demo message
+            message = "Hello World!"u8.ToArray(),
+            // generate message header
+            header = NFraming.Create(message.LongLength);
+
         // create framing instance
         var framing = new NFraming();
 
@@ -27,23 +25,23 @@ public class NFramingTest
         for (var i = 0; i < 10; i++)
         {
             // first deploy
-            framing.Write(new ArraySegment<byte>(MyHeader), NFraming.NewStream);
+            framing.Write(new ArraySegment<byte>(header), NFraming.NewStream);
             var deploy1 = framing.Read(out var stream1);
             Assert.False(deploy1);
             Assert.Null(stream1);
 
             // last deploy
-            framing.Write(new ArraySegment<byte>(MyMessage), NFraming.NewStream);
+            framing.Write(new ArraySegment<byte>(message), NFraming.NewStream);
             var deploy2 = framing.Read(out var stream2);
             Assert.True(deploy2);
             Assert.NotNull(stream2);
-            Assert.Equal(MyMessage.Length, stream2.Length);
+            Assert.Equal(message.Length, stream2.Length);
 
             // validate data
-            var data = new byte[MyMessage.Length];
+            var data = new byte[message.Length];
             Assert.Equal(0, stream2.Position);
-            Assert.Equal(MyMessage.Length, stream2.Read(data, 0, data.Length));
-            Assert.Equal(MyMessage, data);
+            Assert.Equal(message.Length, stream2.Read(data, 0, data.Length));
+            Assert.Equal(message, data);
             Assert.Equal(data.Length, stream2.Position);
         }
     }
@@ -52,6 +50,9 @@ public class NFramingTest
     public void Multiples()
     {
         const int times = 10;
+
+        var random = new Random(Seed: 1024);
+        var messages = new List<byte[]>();
 
         // create buffer instance
         var buffer = new List<byte>();
@@ -62,8 +63,15 @@ public class NFramingTest
         // create multi call message
         for (var i = 0; i < times; i++)
         {
-            buffer.AddRange(MyHeader);
-            buffer.AddRange(MyMessage);
+            // generate random message
+            var message = new byte[random.Next(32, 128)];
+            Random.Shared.NextBytes(message);
+            // generate and send the message header
+            buffer.AddRange(NFraming.Create(message.LongLength));
+            // send message payload
+            buffer.AddRange(message);
+            // save message payload to check
+            messages.Add(message);
         }
 
         framing.Write(new ArraySegment<byte>(buffer.ToArray()), NFraming.NewStream);
@@ -73,16 +81,19 @@ public class NFramingTest
 
         while (framing.Read(out var stream))
         {
-            count++;
+            var message = messages[count];
             Assert.NotNull(stream);
-            Assert.Equal(MyMessage.Length, stream.Length);
+            Assert.Equal(message.Length, stream.Length);
 
             // validate data
-            var data = new byte[MyMessage.Length];
+            var data = new byte[message.Length];
             Assert.Equal(0, stream.Position);
-            Assert.Equal(MyMessage.Length, stream.Read(data, 0, data.Length));
-            Assert.Equal(MyMessage, data);
+            Assert.Equal(message.Length, stream.Read(data, 0, data.Length));
+            Assert.Equal(message, data);
             Assert.Equal(data.Length, stream.Position);
+
+            // increment total count
+            count++;
         }
 
         Assert.Equal(times, count);
