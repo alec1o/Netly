a0a3ab5c9082a5d25137460becd769afd38d3410
Alecio Furanze (Ale) <alecio@kezero.com>
2025-09-10 10:11:49 +0200

    upgrade NHost: implicit conversion from string

--- src/netly/NHost.cs
+++ src/netly/NHost.cs
@@ -1,118 +1,140 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.Sockets;
 
 namespace Netly
 {
     /// <summary>
-    ///     Netly: Host (EndPoint Manager)
+    ///     Represents a network host (IP address + port) and provides multiple constructors and implicit conversions
+    ///     from string. Supports equality comparison and conversion to <see cref="IPEndPoint" />.
     /// </summary>
     public class NHost
     {
         /// <summary>
-        ///     Return default Host instance: (0.0.0.0:0)
+        ///     Default host instance representing 0.0.0.0:0.
         /// </summary>
-        /// <returns>Return default host instance: (0.0.0.0:0)</returns>
         public static readonly NHost Default = new NHost(IPAddress.Any, 0);
 
         /// <summary>
-        ///     Create instance of (host)
+        ///     Creates a new <see cref="NHost" /> from an IP string and port number.
         /// </summary>
-        /// <param name="ip">IPAddress</param>
-        /// <param name="port">Port</param>
+        /// <param name="ip">The IP address as a string.</param>
+        /// <param name="port">The port number.</param>
+        /// <exception cref="FormatException">Thrown if the IP string is not a valid IP address.</exception>
         public NHost(string ip, int port)
         {
-            IPEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+            if (!IPAddress.TryParse(ip, out var address))
+                throw new FormatException($"Invalid IP address: {ip}");
+
+            IPEndPoint = new IPEndPoint(address, port);
         }
 
         /// <summary>
-        ///     Create instance of (host)
+        ///     Creates a new <see cref="NHost" /> from an <see cref="EndPoint" />.
         /// </summary>
-        /// <param name="endpoint">EndPoint</param>
+        /// <param name="endpoint">The endpoint to copy.</param>
         public NHost(EndPoint endpoint)
         {
-            var temp = (IPEndPoint)endpoint;
+            var temp = endpoint as IPEndPoint ?? throw new ArgumentException("EndPoint must be IPEndPoint");
             IPEndPoint = new IPEndPoint(temp.Address, temp.Port);
         }
 
         /// <summary>
-        ///     Create instance of (host)
+        ///     Creates a new <see cref="NHost" /> from an <see cref="IPEndPoint" />.
         /// </summary>
-        /// <param name="ipEndpoint">IPEndPoint</param>
+        /// <param name="ipEndpoint">The IPEndPoint to copy.</param>
         public NHost(IPEndPoint ipEndpoint)
         {
             IPEndPoint = new IPEndPoint(ipEndpoint.Address, ipEndpoint.Port);
         }
 
         /// <summary>
-        ///     Create instance of (host)
+        ///     Creates a new <see cref="NHost" /> from an <see cref="IPAddress" /> and port.
         /// </summary>
-        /// <param name="address">IPAddress</param>
-        /// <param name="port">Port</param>
+        /// <param name="address">The IP address.</param>
+        /// <param name="port">The port number.</param>
         public NHost(IPAddress address, int port)
         {
             IPEndPoint = new IPEndPoint(address, port);
         }
 
         /// <summary>
-        ///     Return IPAddress
+        ///     Returns the <see cref="IPAddress" /> of the host.
         /// </summary>
         public IPAddress Address => IPEndPoint.Address;
 
         /// <summary>
-        ///     Return Port
+        ///     Returns the port number of the host.
         /// </summary>
         public int Port => IPEndPoint.Port;
 
         /// <summary>
-        ///     Return EndPoint
+        ///     Returns the host as an <see cref="EndPoint" />.
         /// </summary>
         public EndPoint EndPoint => IPEndPoint;
 
         /// <summary>
-        ///     Return IPEndPoint
+        ///     Returns the <see cref="IPEndPoint" /> representing this host.
         /// </summary>
         public IPEndPoint IPEndPoint { get; }
 
         /// <summary>
-        ///     Return AddressFamily
+        ///     Returns the address family of the host.
         /// </summary>
         public AddressFamily AddressFamily => IPEndPoint.AddressFamily;
 
+        /// <summary>
+        ///     Returns a string representation of the host as "IP:Port".
+        /// </summary>
         public override string ToString()
         {
             return IPEndPoint.ToString();
         }
 
         /// <summary>
-        ///     Compare Host. Check IP/Port
+        ///     Determines whether the specified object is equal to the current host by comparing IP and port.
         /// </summary>
-        /// <param name="object">Object</param>
-        /// <returns></returns>
-        public override bool Equals(object @object)
+        /// <param name="obj">The object to compare.</param>
+        public override bool Equals(object obj)
         {
-            return Equals(this, @object);
+            return obj is NHost other && Address.Equals(other.Address) && Port == other.Port;
         }
 
         /// <summary>
-        ///     Compare two (2) Host. Check IP/Port
+        ///     Returns a hash code for this host.
         /// </summary>
-        /// <param name="objectA">Object A</param>
-        /// <param name="objectB">Object B</param>
-        /// <returns>Return true if those object is Host and have same value</returns>
-        public new static bool Equals(object objectA, object objectB)
-        {
-            if (objectA == null || objectB == null) return false;
-
-            if (objectA.GetType() == typeof(NHost) && objectB.GetType() == typeof(NHost))
-                return ((NHost)objectA).ToString() == ((NHost)objectB).ToString();
-
-            return false;
-        }
-
         public override int GetHashCode()
         {
-            if (IPEndPoint == null) return 0;
-            return IPEndPoint.GetHashCode();
+            return IPEndPoint == null ? 0 : IPEndPoint.GetHashCode();
+        }
+
+        /// <summary>
+        ///     Implicitly converts a valid IP:Port string to an <see cref="NHost" />.
+        /// </summary>
+        /// <param name="host">The host string (e.g., "127.0.0.1:3300").</param>
+        /// <exception cref="FormatException">Thrown if the string is not a valid IP:Port format.</exception>
+        public static implicit operator NHost(string host)
+        {
+            if (string.IsNullOrWhiteSpace(host))
+                throw new FormatException("Host string is null or empty.");
+
+            // Add temporary scheme for Uri parsing
+            var temp = host.Contains("://") ? host : "tcp://" + host;
+
+            Uri uri;
+            try
+            {
+                uri = new Uri(temp);
+            }
+            catch
+            {
+                throw new FormatException($"Invalid host format: {host}");
+            }
+
+            if (!IPAddress.TryParse(uri.Host, out _))
+                throw new FormatException($"Invalid IP address in host: {host}");
+
+            return new NHost(uri.Host, uri.Port);
         }
     }
 }
